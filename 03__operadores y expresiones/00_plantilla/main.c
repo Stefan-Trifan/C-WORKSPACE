78c48696843a4754001362e45a8dc41651f14149
Stefan <trfstephan@gmail.com>
2024-09-30 11:27:37 +0200

    operaciones relacionales

--- 03__operadores y expresiones/00_plantilla/main.c
+++ 03__operadores y expresiones/00_plantilla/main.c
@@ -11,7 +11,7 @@
 		limpiar()
 
 *	Enunciado
-		LoremIpsumDummyText
+		* LoremIpsumDummyText
  */
 
 #include <stdio.h>
---
+++ 03__operadores y expresiones/06_operadores_relacionales/main.c
@@ -0,0 +1,44 @@
+/*
+*	Autor
+		Stefan Trifan
+
+*	Compilar
+		mac    : gcc main.c -o main
+		windows: gcc main.c -o main.exe
+			   : ./main.c
+
+*	Introducir después de scanf
+		limpiar()
+
+*	Enunciado
+		* Generar un programa que pida un carácter por pantalla, y que compruebe si es una letra tanto mayúscula como minúscula. 
+		* Almacenará el resultado en una variable entera y lo mostrará por pantalla. 
+		* No se usarán operadores "if/then", sólo se almacena el resultado de la comprobación.
+ */
+
+#include <stdio.h>
+
+// Llamamos con limpiar()
+void limpiar(){
+	while(getchar() != '\n');
+}
+
+int main(){
+
+	char letra       = ' ';
+	int  esMinuscula = 0;
+	int  esMayuscula = 0;
+
+	// Pedimos la letra
+	printf("Por favor introduce una letra (a-z, A-Z)\n");
+	scanf("%c", &letra);
+
+	// Comprobamos si es letra mayuscula o minuscula
+	esMinuscula = (letra >= 'a') && (letra <= 'z');
+	esMayuscula = (letra >= 'A') && (letra <= 'Z');
+	
+	// Devolvemos el resultado
+	printf(
+	"\tLa letra es minúscula: %i\n"
+	"\tLa letra es mayúscula: %i\n", esMinuscula, esMayuscula);
+}
