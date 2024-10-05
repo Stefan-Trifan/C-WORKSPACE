3e0d0ea8960452213cd14500648c2cb2c5f5b0bd
Stefan <trfstephan@gmail.com>
2024-10-05 13:49:33 +0200

    correcciones

--- 02__tipos_datos/03_asci/comprobar_valores.c
+++ 02__tipos_datos/03_asci/comprobar_valores.c
@@ -2,22 +2,32 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <float.h>
+
 int main(int argc, char** argv) {
+    // Información sobre char
     printf("El numero de bits de un char es        :   %d\n", CHAR_BIT);
     printf("El numero de bytes de un char es       :   %d\n", sizeof(char));
-    printf("El menor valor de un signed char es    :   %d\n", sizeof(char));
-    printf("El mayor valor de un signed char es    :   %d\n", CHAR_MIN);
-    printf("El mayor valor de un unsigned char es  :   %d\n", CHAR_MAX);
-    printf("El numero de bytes de un int es        :   %d\n\n", UCHAR_MAX);
-    printf("El menor valor de un int es            :   %d\n", sizeof(int));
-    printf("El mayor valor de un int es            :   %d\n", INT_MIN);
-    printf("El mayor valor de un unsigned int es   :   %d\n", INT_MAX);
-    printf("El numero de bytes de un short es      :   %u\n\n", (unsigned int) UINT_MAX);
-    printf("El menor valor de un short es          :   %d\n", sizeof(short));
-    printf("El mayor valor de un short es          :   %d\n", sizeof(short));
-    printf("El mayor valor de un unsigned short es :   %d\n", SHRT_MIN);
-    printf("El numero de bytes de un long es       :   %d\n\n", (unsigned short) USHRT_MAX);
-    printf("El mayor valor de un long es           :   %d\n", sizeof(long));
+    printf("El menor valor de un signed char es    :   %d\n", CHAR_MIN);
+    printf("El mayor valor de un signed char es    :   %d\n", CHAR_MAX);
+    printf("El mayor valor de un unsigned char es  :   %d\n\n", UCHAR_MAX);
+
+    // Información sobre int
+    printf("El numero de bytes de un int es        :   %d\n", sizeof(int));
+    printf("El menor valor de un int es            :   %d\n", INT_MIN);
+    printf("El mayor valor de un int es            :   %d\n", INT_MAX);
+    printf("El mayor valor de un unsigned int es   :   %u\n\n", (unsigned int) UINT_MAX);
+
+    // Información sobre short
+    printf("El numero de bytes de un short es      :   %d\n", sizeof(short));
+    printf("El menor valor de un short es          :   %d\n", SHRT_MIN);
+    printf("El mayor valor de un short es          :   %d\n", SHRT_MAX);
+    printf("El mayor valor de un unsigned short es :   %d\n\n", (unsigned short) USHRT_MAX);
+
+    // Información sobre long
+    printf("El numero de bytes de un long es       :   %d\n", sizeof(long));
+    printf("El menor valor de un long es           :   %ld\n", (long) LONG_MIN);
+    printf("El mayor valor de un long es           :   %ld\n", (long) LONG_MAX);
     printf("El mayor valor de un unsigned long es  :   %lu\n", (unsigned long) ULONG_MAX);
+
     return 0;
 }
