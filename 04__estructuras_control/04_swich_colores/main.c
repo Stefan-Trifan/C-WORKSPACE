7f06aef6e2f26f5efed2a887357f4dcf6bf05f15
Stefan <trfstephan@gmail.com>
2024-10-11 14:25:55 +0200

    ejercicio while

---
+++ 04__estructuras_control/04_swich_colores/main.c
@@ -0,0 +1,51 @@
+/*
+*	Autor
+		Stefan Trifan
+
+*	Compilar
+		mac     : gcc main.c -o main
+		windows : gcc main.c -o main.exe
+			    : ./main.exe
+
+*	Introducir después de scanf
+		limpiar()
+
+*	Enunciado
+		Pide un carácter b, r, n, v y devuelve un color que empieza por ese carácter o un mensaje indicando que no hay ningún color asignado. Realizar el ejemplo mediante un switch-case.
+ */
+
+#include <stdio.h>
+
+// Llamamos con limpiar()
+void limpiar(){
+	while(getchar() != '\n');
+}
+
+int main(){
+
+	// Decaramos las variables
+	char letra;
+
+	printf("Introduce la letra 'b', 'r', 'n', 'v'\n");
+	scanf("%c", &letra);
+	limpiar();
+
+	switch(letra){
+		case 'b': printf("Tu color es Bazul");
+				break;
+		case 'B': printf("Tu color es Bazul");
+				break;
+		case 'r': printf("Tu color es Ramarillo");
+				break;
+		case 'R': printf("Tu color es Ramarillo");
+				break;
+		case 'n': printf("Tu color es Niligo");
+				break;
+		case 'N': printf("Tu color es Niligo");
+				break;
+		case 'v': printf("Tu color es Varrón");
+				break;
+		case 'V': printf("Tu color es Varrón");
+				break;
+	}
+}
---
+++ 04__estructuras_control/05_while_nr-caracteres/main.c
@@ -0,0 +1,40 @@
+/*
+*	Autor
+		Stefan Trifan
+
+*	Compilar
+		mac     : gcc main.c -o main
+		windows : gcc main.c -o main.exe
+			    : ./main.exe
+
+*	Introducir después de scanf
+		limpiar()
+
+*	Enunciado
+		Pide un número por pantalla, cuenta los caracteres que tiene y devuelve el resultado
+ */
+
+#include <stdio.h>
+
+// Llamamos con limpiar()
+void limpiar(){
+	while(getchar() != '\n');
+}
+
+int main(){
+
+	// Declaramos las variables
+	int numero = 0;
+	int caracteres = 0;
+
+	// Pedimos el número
+	printf("Introduce un número del 0 al 999999999\n");
+	scanf("%i", &numero);
+
+	while( numero > 0 ){
+		numero = numero / 10;
+		caracteres = caracteres + 1;
+	}
+
+	printf("El número de caracteres es %i", caracteres);
+}
