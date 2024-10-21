48fa796f76cadd763b8512eda6b762cfbdacca6f
Stefan <trfstephan@gmail.com>
2024-10-21 19:02:08 +0200

    ejercicio programación

--- 000_entregas/01_Practica1_operadores_expresiones/Stefan_Trifan_Practica1_IPR1/ejercicio-E_v1.0.0/main.c
+++ 000_entregas/01_Practica1_operadores_expresiones/Stefan_Trifan_Practica1_IPR1/ejercicio-E_v1.0.0/main.c
@@ -23,24 +23,50 @@ void limpiar(){
 	while(getchar() != '\n');
 }
 
+// Transformamos el número a binario
+void printBinary(int number, int positions) {
+	// Assuming a 32-bit integer
+	unsigned int mask = 1 << (positions - 1); // Start with the leftmost bit
+	int i = 0;
+	while (i < positions) {
+		// Print '1' or '0' based on the current bit
+		if (number & mask) {
+			printf("1");
+		} else {
+			printf("0");
+		}
+		// Shift the mask to the right by one bit
+		mask >>= 1;
+		i++;
+	}
+	printf("\n");
+}
+
 int main(){
 
 	// Declaramos las variables del programa
-	float parte4 = 0;
-	float parte3 = 0;
-	float parte2 = 0;
-	float parte1 = 0;
+	unsigned int miEntero  = 0;
+	unsigned int miBinario = 0;
+
+	unsigned int parte4 = 0;
+	unsigned int parte3 = 0;
+	unsigned int parte2 = 0;
+	unsigned int parte1 = 0;
 
 	// Pedimos el número de 32 bits al usuario
+	printf("Ingrese un número entero de 32 bits: ");
+	scanf("%u", &miEntero);
+	limpiar();
 
-	// Transformamos el número a binario
+	// Convertimos el número a binario
+	miBinario = printBinary(miEntero, 8);
 
 	// Descomponemos el número en 4 partes de 8 números
 
 	// Imprimos cada parte en binario
-	printf("Parte 4 (bits 31-24): %f\n", parte4);
-	printf("Parte 3 (bits 23-16): %f\n", parte3);
-	printf("Parte 2 (bits 15-08): %f\n", parte2);
-	printf("Parte 1 (bits 07-00): %f\n", parte1);
+	printf("Parte 4 (bits 31-24): %u\n", parte4);
+	printf("Parte 3 (bits 23-16): %u\n", parte3);
+	printf("Parte 2 (bits 15-08): %u\n", parte2);
+	printf("Parte 1 (bits 07-00): %u\n", parte1);
 		
 }
