01bfc9c1823355b56a2617fea69ca034ff46139f
Stefan <trfstephan@gmail.com>
2024-10-25 14:18:11 +0200

    funciones

--- 05__funciones/00_plantilla/main.c
+++ 05__funciones/00_plantilla/main.c
@@ -16,6 +16,8 @@
 
 #include <stdio.h>
 
+int multiplica(int primerNumero, int segundoNumero);
+
 // Llamamos con limpiar()
 void limpiar(){
 	while(getchar() != '\n');
@@ -23,4 +25,24 @@ void limpiar(){
 
 int main(){
 
+	int num1 = 0;
+	int num2 = 0;
+	
+
+	printf("Introduce el primer número: ");
+	scanf("%i", &num1);
+
+	printf("Introduce el segundo número: ");
+	scanf("%i", &num2);
+
+	int resultado = multiplica(num1, num2);
+	printf("El resultado de la multiplicación es: %i\n", resultado);
+
+}
+
+int multiplica(int primerNumero, int segundoNumero){
+	int resultado = 0;
+
+	resultado = primerNumero * segundoNumero;
+	return resultado;
 }
