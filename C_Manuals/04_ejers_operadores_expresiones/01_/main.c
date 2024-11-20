ecdeac3bce5d979679c6256632170ec9b76fad64
Stefan <trfstephan@gmail.com>
2024-11-20 11:04:46 +0100

    add new main.c files for exercises 00 to 10 with compilation instructions

---
+++ C_Manuals/04_ejers_operadores_expresiones/01_/main.c
@@ -0,0 +1,48 @@
+/*
+	*	Autor
+		Stefan Trifan
+
+	* 	Compilar
+		mac     : comp
+				  run
+				  brun
+		windows : gcc main.c -o main.exe
+				: ./main.exe
+
+	* 	Enunciado X
+		LoremIpsumDummyText
+*/
+
+/* _________________________________________
+   Inicio cabecera */
+
+#include <stdio.h>
+
+void limpiarBuffer();
+
+/* _________________________________________
+   Inicio main() */
+
+
+int main(){
+	printf("\n_________________________________________START\n\n");
+
+	char nombre[40];
+	int salario;
+    
+    printf("Introduce tu nombre: \n");
+	gets(nombre);
+	printf("Tu nombre es: %s", nombre);
+    
+	printf("\n_________________________________________END\n\n");
+	return 0;
+}
+
+
+
+/* _________________________________________
+   Inicio definicion de funciones */
+
+void limpiarBuffer(){
+	while (getchar() != '\n');
+}
---
+++ C_Manuals/06_ejers_estructuras-seleccion/00_plantilla/main.c
@@ -0,0 +1,40 @@
+/*
+	*	Autor
+		Stefan Trifan
+
+	* 	Compilar
+		mac     : comp
+				  run
+				  brun
+		windows : gcc main.c -o main.exe
+				: ./main.exe
+
+	* 	Enunciado X
+		LoremIpsumDummyText
+*/
+
+/* _________________________________________
+   Inicio cabecera */
+
+#include <stdio.h>
+
+void limpiarBuffer();
+
+/* _________________________________________
+   Inicio main() */
+
+int main(){
+	printf("\n_________________________________________START\n\n");
+    
+    
+    
+	printf("\n_________________________________________END\n\n");
+	return 0;
+}
+
+/* _________________________________________
+   Inicio definicion de funciones */
+
+void limpiarBuffer(){
+	while (getchar() != '\n');
+}
