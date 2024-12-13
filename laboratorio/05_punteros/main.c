ddf5476310939ef2c88de47492755b96cd7de212
Stefan <trfstephan@gmail.com>
2024-12-13 14:07:45 +0100

    remove: delete outdated main.c files and add new implementations for pointer operations and list handling

--- laboratorio/05_punteros/main.c
+++ laboratorio/05_punteros/main.c
@@ -28,7 +28,32 @@ void limpiarBuffer();
 int main(){
 	printf("\n_________________________________________START\n\n");
     
-    
+	// Primer puntero
+    int var1 = 3;
+	int *pvar1 = &var1;
+	printf("%i\n", var1);
+
+	// Segundo puntero
+	int casa = 0;
+	int* direccion;
+	direccion = &casa;
+
+	int* direccion2;
+	direccion2 = direccion;
+
+	printf("Casa: %i direccion: %i direccion2: %i\n", casa, *direccion, *direccion2);
+	casa = 1;
+	printf("Casa: %i direccion: %i direccion2: %i\n", casa, *direccion, *direccion2);
+
+	
+	int var3, var4;
+	int *p;
+	
+	var3 = 5;
+	p = &var3;
+	var4 = *p;
+	*p =10;
+	printf("%i", var3);
     
 	printf("\n_________________________________________END\n\n");
 	return 0;
---
+++ utad/temario/07__punteros/01_puntero/main.c
@@ -0,0 +1,65 @@
+/*
+	*	Autor
+		Stefan Trifan
+
+	* 	Compilar
+		mac     : comp
+				  run
+				  brun
+		windows : gcc main.c -o main.exe
+				: ./main.exe
+
+	*   Estado: todo
+
+	* 	Enunciado X
+		LoremIpsumDummyText
+*/
+
+/* _________________________________________
+   Inicio cabecera */
+
+#include <stdio.h>
+
+void limpiarBuffer();
+
+/* _________________________________________
+   Inicio main() */
+
+int main(){
+	printf("\n_________________________________________START\n\n");
+    
+    int num1 = 0;
+	int *pNum1 = NULL;
+
+	num1 = 0;
+	pNum1 = &num1;
+    
+	printf("El numero guardado es:                            %d\n", num1);
+	printf("La direccion de memoria de var1 es:               %X\n", &num1);
+	printf("La direccion de memoria a la que apunta pNum1 es: %X\n", pNum1);
+	printf("La direccion de memoria de pNum1 es:              %X\n", &pNum1);
+	printf("El contenido de pNum1 es:                         %d\n", *pNum1);
+
+	// INCREMENTAMOS EL CONTENIDO DE PNUM!
+	*pNum1 += 1;
+
+	printf("\n\nDespues de incrementar, el numero guardado es:      %d\n", num1);
+	printf("La direccion de memoria de var1 es:                     %X\n", &num1);
+	printf("La direccion de memoria a la que apunta pNum1 es:       %X\n", pNum1);
+	printf("La direccion de memoria de pNum1 es:                    %X\n", &pNum1);
+	printf("El contenido de pNum1 es:                               %d\n", *pNum1);
+
+
+
+	printf("");
+
+	printf("\n_________________________________________END\n\n");
+	return 0;
+}
+
+/* _________________________________________
+   Inicio definicion de funciones */
+
+void limpiarBuffer(){
+	while (getchar() != '\n');
+}
