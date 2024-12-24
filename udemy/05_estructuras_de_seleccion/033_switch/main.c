5aaafdbe8944bc4a5d8aa29736df290b5065c2fa
Stefan <trfstephan@gmail.com>
2024-12-24 13:15:41 +0100

    eliminar archivo main.c de la carpeta 033; agregar nuevo archivo main.c en la carpeta 033_switch con estructura básica y selección de días de la semana

--- udemy/05_estructuras_de_seleccion/033_switch/main.c
+++ udemy/05_estructuras_de_seleccion/033_switch/main.c
@@ -27,8 +27,31 @@ void limpiarBuffer();
 
 int main(){
 	printf("\n_________________________________________START\n\n");
-    
-    
+
+	// Solicitar al usuario que seleccione un día de la semana
+	int dia;
+
+	printf("Selecciona el dia de la semana como numero (del 1 al 7): ");
+	scanf("%i", &dia);
+
+	switch (dia){
+	case 1: printf("Seleccionaste el Lunes");
+		break;
+	case 2: printf("Seleccionaste el Martes");
+		break;
+	case 3: printf("Seleccionaste el Miercoles");
+		break;
+	case 4: printf("Seleccionaste el Jueves");
+		break;
+	case 5: printf("Seleccionaste el Viernes");
+		break;
+	case 6: printf("Seleccionaste el Sabado");
+		break;
+	case 7: printf("Seleccionaste el Domingo");
+		break;
+	default: printf("Esta opcion no es valida");
+		break;
+	}
     
 	printf("\n_________________________________________END\n\n");
 	return 0;
