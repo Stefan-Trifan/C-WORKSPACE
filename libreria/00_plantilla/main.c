3e4e29cbb8f5ac6e1c248e6bad3f853485c7a457
Stefan <trfstephan@gmail.com>
2024-12-26 11:19:29 +0100

    agregar nuevos archivos main.c para plantillas y funciones de manejo de matrices

--- libreria/00_plantilla/main.c
+++ libreria/00_plantilla/main.c
@@ -19,7 +19,6 @@
    Inicio cabecera */
 
 #include <stdio.h>
-#include <string.h>
 
 void clearBuffer();
 
@@ -28,25 +27,8 @@ void clearBuffer();
 
 int main(){
 	printf("\n_________________________________________START\n\n");
-
-	char nombre[20] = "";
-	char signo[20] = "";
     
-    printf("Introduce tu nombre: ");
-	fgets(nombre, 20, stdin);
-	strtok(nombre, "\n");
-	printf("Tu nombre es: %s\n", nombre);
-
-	printf("Introduce tu signo: ");
-	fgets(signo, 20, stdin);
-	strtok(signo, "\n");
-	printf("Tu signo es: %s\n", signo);
-
-	if(strcmp("Aries", signo) == 0 || strcmp("aries", signo) == 0 || strcmp("ARIES", signo) == 0){
-		printf("\nTu signo es Aries");
-	} else {
-		printf("\nTu signo no es Aries");
-	};
+    
     
 	printf("\n_________________________________________END\n\n");
 	return 0;
