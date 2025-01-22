1cf1ce60caf0d7590deb3b85d33b3ff88d87cbaf
Stefan <trfstephan@gmail.com>
2025-01-22 20:21:15 +0100

    agregar funciones para operaciones matemáticas y menú interactivo en main.c

--- udemy/11_/097_operaciones_con_funciones/main.c
+++ udemy/11_/097_operaciones_con_funciones/main.c
@@ -20,7 +20,12 @@
 
 #include <stdio.h>
 
-void clearBuffer();
+void  menu        (int *opcion, float *num1, float *num2);
+float sumar       (float num1, float num2);
+float restar      (float num1, float num2);
+float multiplicar (float num1, float num2);
+float dividir     (float num1, float num2);
+void  clearBuffer ();
 
 /* _________________________________________
    Inicio main() */
@@ -28,8 +33,31 @@ void clearBuffer();
 int main()
 {
 	printf("\n_________________________________________START\n\n");
+
+	int   opcion    = 0;
+	float num1      = 0,
+	      num2      = 0,
+	      resultado = 0;
+
+	menu(&opcion, &num1, &num2);
+
+	switch (opcion)
+	{
+		case 1: 
+			resultado = sumar(num1, num2);
+			break;
+		case 2: 
+			resultado = restar(num1, num2);
+			break;
+		case 3: 
+			resultado = multiplicar(num1, num2);
+			break;
+		case 4: 
+			resultado = dividir(num1, num2);
+			break;
+	}
     
-    
+	printf("El resultado es: %f", resultado);
     
 	printf("\n_________________________________________END\n\n");
 	return 0;
@@ -37,6 +65,47 @@ int main()
 
 /* _________________________________________
    Inicio definicion de funciones */
+
+void menu(int *opcion, float *num1, float *num2)
+{
+	do {
+		printf(
+			"Introduce el tipo de operacion: \n"
+			"1. Sumar\n"
+			"2. Restar\n"
+			"3. Multiplicar\n"
+			"4. Dividir\n"
+			"-> ");
+		scanf("%i", opcion);
+		printf("\n");
+	} while (*opcion < 1 || *opcion > 4);
+
+	printf("Introduce el numero 1: ");
+	scanf("%f", num1);
+	printf("Introduce el numero 2: ");
+	scanf("%f", num2);
+}
+
+float sumar(float num1, float num2)
+{
+	return num1 + num2;
+}
+
+float restar(float num1, float num2)
+{
+	return num1 - num2;
+}
+
+float multiplicar(float num1, float num2)
+{
+	return num1 * num2;
+}
+
+float dividir(float num1, float num2)
+{
+	return num1 / num2;
+}
+
 
 void clearBuffer()
 {
