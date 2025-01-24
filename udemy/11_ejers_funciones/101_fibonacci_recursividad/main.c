9517245700b2c11d566c84578cbd31957d0d5908
Stefan <trfstephan@gmail.com>
2025-01-24 01:23:35 +0100

    eliminar archivo main.c y agregar implementación de la serie de Fibonacci con entrada del usuario

---
+++ udemy/11_ejers_funciones/101_fibonacci_recursividad/main.c
@@ -0,0 +1,121 @@
+/*
+	*	Autor
+		Stefan Trifan
+
+	* 	Compilar
+		mac     : comp (compilar)
+				  run  (ejecutar)
+				  brun (compilar y ejecutar)
+		windows : gcc main.c -o main.exe
+				: ./main.exe
+
+	*   Estado: 
+
+	* 	Enunciado X
+		
+*/
+
+/* _________________________________________
+   Inicio cabecera */
+
+#include <stdio.h>
+
+int fibona(int numElementos);
+void clearBuffer();
+
+/* _________________________________________
+   Inicio main() */
+
+int main()
+{
+	printf("\n_________________________________________START\n\n");
+    
+    int numElementos = 0, esValido = 1;
+
+	printf("Cuantos elementos quieres: ");
+	do
+	{
+		if(esValido != 1)
+		{
+			printf("\033[1;31mERROR: El tipo de dato introducido no es válido. Por favor, inténtelo de nuevo: \033[0m");
+		}
+		esValido = scanf("%i", &numElementos);
+		clearBuffer();
+	} 
+	while (esValido != 1);
+
+	for(int i = 0; i <= numElementos; i++)
+	{
+		printf("%i ", fibona(i));
+	}
+    
+	printf("\n_________________________________________END\n\n");
+	return 0;
+}
+
+/* _________________________________________
+   Inicio definicion de funciones */
+
+/* 
+	0 1 1 2 3 4 8
+	a_n = a_{n - 1} + a_{n - 2}
+	fibona(n) = fibona(n - 1) + fibona(n - 2)
+*/
+
+int fibona(int numElementos)
+{
+	if(numElementos == 0 || numElementos == 1)
+	{
+		return numElementos;
+	}
+	
+	return fibona(numElementos - 1) + fibona(numElementos - 2);
+}
+
+/*  
+	fib(0): 
+		-> 0  // Caso base
+
+	fib(1): 
+		-> 1  // Caso base
+
+	fib(2): 
+		-> fib(1) + fib(0) 
+		
+		-> 1 + 0 -> 1  
+
+	fib(3): 
+		-> fib(2) + fib(1) 
+		-> fib(1) + fib(0) + fib(1) 
+
+		-> 1 + 0 + 1 -> 2  
+
+	fib(4): 
+		-> fib(3) + fib(2)  
+	    -> (fib(2) + fib(1)) + (fib(1) + fib(0))  
+	    -> ((fib(1) + fib(0)) + fib(1)) + (fib(1) + fib(0))  
+	
+	    -> ((1 + 0) + 1) + (1 + 0) -> 2 + 1 -> 3
+
+	fib(5): 
+		-> fib(4) + fib(3)  
+		-> (fib(3) + fib(2)) + (fib(2) + fib(1))  
+		-> ((fib(2) + fib(1)) + (fib(1) + fib(0))) + ((fib(1) + fib(0)) + fib(1))  
+		-> (((fib(1) + fib(0)) + fib(1)) + (fib(1) + fib(0))) + ((fib(1) + fib(0)) + fib(1))
+
+		-> (((1 + 0) + 1) + (1 + 0)) + ((1 + 0) + 1) -> (2 + 1) + (1 + 1) -> 3 + 2 -> 5
+
+	fib(6): 
+		-> fib(5) + fib(4)   
+		-> (fib(4) + fib(3)) + (fib(3) + fib(2))  
+		-> ((fib(3) + fib(2)) + (fib(2) + fib(1))) + ((fib(2) + fib(1)) + (fib(1) + fib(0)))  
+		-> (((fib(2) + fib(1)) + (fib(1) + fib(0))) + ((fib(1) + fib(0)) + fib(1)))  
+
+		-> (((1 + 0) + 1) + (1 + 0)) + ((1 + 0) + 1) -> 5 + 3 -> 8
+*/
+
+
+void clearBuffer()
+{
+	while (getchar() != '\n');
+}
