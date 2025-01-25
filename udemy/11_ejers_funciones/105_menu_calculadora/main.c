819f437ef7814a4cff26927afabffed6769a6791
Stefan <trfstephan@gmail.com>
2025-01-25 16:20:10 +0100

    agregar implementación de una calculadora con operaciones básicas

---
+++ udemy/11_ejers_funciones/105_menu_calculadora/main.c
@@ -0,0 +1,179 @@
+/*
+	*	Autor
+		Stefan Trifan
+
+	* 	Compilar
+		mac     : comp (compilar)
+				  run  (ejecutar)
+				  brun (compilar y ejecutar)
+		windows : gcc main.c -o main.exe
+				: ./main.exe
+
+	*   Estado: 
+
+	* 	Enunciado X
+		LoremIpsumDummyText
+*/
+
+/* _________________________________________
+   Inicio cabecera */
+
+#include <stdio.h>
+
+float solicitarFloat();
+int   solicitarInt();
+void  menuCalculadora();
+void  sumar();
+void  restar();
+void  multiplicar();
+void  dividir();
+void  clearBuffer();
+
+/* _________________________________________
+   Inicio main() */
+
+int main()
+{
+	printf("\n_________________________________________START\n\n");
+
+	menuCalculadora();
+    
+	printf("\n_________________________________________END\n\n");
+	return 0;
+}
+
+/* _________________________________________
+   Inicio definicion de funciones */
+
+float solicitarFloat()
+{
+	float num = 0;
+	int esValido = 1;
+	printf("-> ");
+	do
+	{	
+		if(esValido == 0)
+		{
+			printf(
+				"\033[1;31mERROR: El tipo de dato introducido no es válido. Por favor, inténtelo de nuevo. \n\033[0m"
+				"\033[1;31m-> \033[0m"
+			);
+		}
+		esValido = scanf("%f", &num);
+		clearBuffer();
+	} 
+	while (esValido != 1);
+	return num;
+}
+
+int solicitarInt()
+{
+	int num = 0, esValido = 1;
+	printf("-> ");
+	do
+	{	
+		if(esValido == 0)
+		{
+			printf(
+				"\033[1;31mERROR: El tipo de dato introducido no es válido. Por favor, inténtelo de nuevo. \n\033[0m"
+				"\033[1;31m-> \033[0m"
+			);
+		}
+		esValido = scanf("%i", &num);
+		clearBuffer();
+	} 
+	while (esValido != 1);
+	return num;
+}
+
+void menuCalculadora()
+{
+	int opcion = 0;
+	int resultado = 0;
+
+	do
+	{
+		printf(
+			"Introduce una opcion a realizar: \n"
+			"1. Sumar\n"
+			"2. Restar\n"
+			"3. Multiplicar\n"
+			"4. Dividir\n"
+			"5. Salir\n");
+		opcion = solicitarInt();
+
+		switch (opcion)
+		{
+			case 1: 
+				sumar();
+				break;
+			case 2: 
+				restar();
+				break;
+			case 3: 
+				multiplicar();
+				break;
+			case 4: 
+				dividir();
+				break;
+			case 5: 
+				break;
+			default: 
+				break;
+		}
+	} 
+	while (opcion != 5);
+}
+
+void sumar()
+{
+	float num1 = 0, num2 = 0, resultado = 0;
+	printf("Introduce el primer numero: \n");
+	num1 = solicitarFloat();
+	printf("Introduce el segundo numero: \n");
+	num2 = solicitarFloat();
+	resultado = num1 + num2;
+	printf("%f + %f = %f\n\n", num1, num2, resultado);
+
+}
+
+void restar()
+{
+	float num1 = 0, num2 = 0, resultado = 0;
+	printf("Introduce el primer numero: \n");
+	num1 = solicitarFloat();
+	printf("Introduce el segundo numero: \n");
+	num2 = solicitarFloat();
+	resultado = num1 - num2;
+	printf("%f - %f = %f\n\n", num1, num2, resultado);
+
+}
+
+void multiplicar()
+{
+	float num1 = 0, num2 = 0, resultado = 0;
+	printf("Introduce el primer numero: \n");
+	num1 = solicitarFloat();
+	printf("Introduce el segundo numero: \n");
+	num2 = solicitarFloat();
+	resultado = num1 * num2;
+	printf("%f * %f = %f\n\n", num1, num2, resultado);
+
+}
+
+void dividir()
+{
+	float num1 = 0, num2 = 0, resultado = 0;
+	printf("Introduce el primer numero: \n");
+	num1 = solicitarFloat();
+	printf("Introduce el segundo numero: \n");
+	num2 = solicitarFloat();
+	resultado = num1 / num2;
+	printf("%f / %f = %f\n\n", num1, num2, resultado);
+
+}
+
+void clearBuffer()
+{
+	while (getchar() != '\n');
+}
