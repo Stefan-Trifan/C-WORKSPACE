5fdcbb14a6b406e47c437bc52497e0634b97be95
Stefan <trfstephan@gmail.com>
2025-01-26 19:20:56 +0100

    agregar nueva implementación de conversión a binario en main.c

--- udemy/12_ejers_recursividad/116_conversion_a_binario/main.c
+++ udemy/12_ejers_recursividad/116_conversion_a_binario/main.c
@@ -19,7 +19,8 @@
    Inicio cabecera */
 
 #include <stdio.h>
-
+void convertirBinario(int num);
+int pedirEntero();
 void clearBuffer();
 
 /* _________________________________________
@@ -28,8 +29,12 @@ void clearBuffer();
 int main()
 {
 	printf("\n_________________________________________START\n\n");
+
+	int num = 0;
     
-    
+    printf("Introduce un numero \n");
+	num = pedirEntero();
+	convertirBinario(num);
     
 	printf("\n_________________________________________END\n\n");
 	return 0;
@@ -37,6 +42,37 @@ int main()
 
 /* _________________________________________
    Inicio definicion de funciones */
+
+void convertirBinario(int num)
+{
+	if(num > 0)
+	{
+		convertirBinario(num / 2);
+		printf("%i", num % 2);
+	}
+
+}
+
+int pedirEntero()
+{
+	int num = 0, esValido = 1;
+	printf("-> ");
+	do
+	{
+		esValido = scanf("%i", &num);
+		clearBuffer();
+		if (esValido == 0)
+		{
+			printf(
+				"\033[1;31mERROR: El tipo de dato introducido no es válido. Por favor, inténtelo de nuevo. \n\033[0m"
+				"\033[1;31m-> \033[0m"
+			);
+			esValido = 0;
+		}
+	}
+	while (esValido != 1);
+	return num;
+}
 
 void clearBuffer()
 {
