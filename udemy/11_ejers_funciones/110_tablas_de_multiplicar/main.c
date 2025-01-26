947258ac788c968b358b799f6cb668bdccc920e1
Stefan <trfstephan@gmail.com>
2025-01-26 14:56:25 +0100

    agregar nueva implementación para la tabla de multiplicar

--- udemy/11_ejers_funciones/110_tablas_de_multiplicar/main.c
+++ udemy/11_ejers_funciones/110_tablas_de_multiplicar/main.c
@@ -20,6 +20,8 @@
 
 #include <stdio.h>
 
+int solicitarEntero();
+void verTabla(int num);
 void clearBuffer();
 
 /* _________________________________________
@@ -28,8 +30,13 @@ void clearBuffer();
 int main()
 {
 	printf("\n_________________________________________START\n\n");
+
+	int num = 0;
     
-    
+    printf("Introduce un numero \n");
+	num = solicitarEntero();
+
+	verTabla(num);
     
 	printf("\n_________________________________________END\n\n");
 	return 0;
@@ -37,6 +44,35 @@ int main()
 
 /* _________________________________________
    Inicio definicion de funciones */
+
+int solicitarEntero()
+{
+	int num = 0, esValido = 1;
+	printf("-> ");
+	do
+	{
+		esValido = scanf("%i", &num);
+		clearBuffer();
+		if(esValido != 1)
+		{
+			printf(
+				"\033[1;31mERROR: El tipo de dato introducido no es válido. Por favor, inténtelo de nuevo. \n\033[0m"
+				"\033[1;31m-> \033[0m"
+			);
+			esValido = 0;
+		}
+	} 
+	while (esValido != 1);
+	return num;
+}
+
+void verTabla(int num)
+{
+	for(int i = 0; i <= 20; i++)
+	{
+		printf("%i x %i = %i\n", num, i, num*i);
+	}
+}
 
 void clearBuffer()
 {
