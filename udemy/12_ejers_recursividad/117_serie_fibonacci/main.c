229515f44c6dbe982bdd2f9d1e28537075b68a54
Stefan <trfstephan@gmail.com>
2025-01-26 22:21:39 +0100

    agregar archivos main.c vacíos para nuevos ejercicios

---
+++ udemy/12_ejers_recursividad/117_serie_fibonacci/main.c
@@ -0,0 +1,90 @@
+/*
+	*	Autor
+		Stefan Trifan
+
+	* 	Compilar
+		mac     : comp (compilar)
+				  run  (ejecutar)
+				  brun (compilar y ejecutar)
+		windows : gcc main.c -o main.exe
+				: ./main.exe
+
+	*   Estado: 
+
+	* 	Enunciado X
+		LoremIpsumDummyText
+*/
+
+/* _________________________________________
+   Inicio cabecera */
+
+#include <stdio.h>
+int  pedirEntero();
+void clearBuffer();
+int  fibonacci(int num);
+
+/* _________________________________________
+   Inicio main() */
+
+int main()
+{
+	printf("\n_________________________________________START\n\n");
+
+	int num = 0, resultado = 0;
+    
+    printf("Introduce un numero \n");
+	num = pedirEntero();
+
+	for(int i = 0; i <= num; i++)
+	{
+		resultado = fibonacci(i);
+		if(i == num)
+			printf("%i", resultado);
+		else
+			printf("%i, ", resultado);
+		
+	}
+
+    
+	printf("\n_________________________________________END\n\n");
+	return 0;
+}
+
+/* _________________________________________
+   Inicio definicion de funciones */
+
+int fibonacci(int num)
+{
+	if(num == 0)
+		return 0; 
+	else if(num == 1)
+		return 1; 
+	else 
+		return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+int pedirEntero()
+{
+	int num = 0, esValido = 1;
+	printf("-> ");
+	do
+	{
+		esValido = scanf("%i", &num);
+		clearBuffer();
+		if (esValido == 0)
+		{
+			printf(
+				"\033[1;31mERROR: El tipo de dato introducido no es válido. Por favor, inténtelo de nuevo. \n\033[0m"
+				"\033[1;31m-> \033[0m"
+			);
+			esValido = 0;
+		}
+	}
+	while (esValido != 1);
+	return num;
+}
+
+void clearBuffer()
+{
+	while (getchar() != '\n');
+}
