61c59152aea5929e98cfb6ce8718934af263c0df
Stefan <trfstephan@gmail.com>
2025-01-28 12:00:51 +0100

    agregar nuevos archivos de plantilla y ejemplos de programas en C, incluyendo conversiones y estructuras de control

---
+++ 01_u_temario/_cuatri1/07__punteros/10_/main.c
@@ -0,0 +1,44 @@
+/*
+	*	Autor
+		Stefan Trifan
+
+	* 	Compilar
+		mac     : comp (compilar)
+				  run  (ejecutar)
+				  brun (compilar y ejecutar)
+		windows : gcc main.c -o main.exe
+				: ./main.exe
+
+	*   Estado: 
+
+	* 	Enunciado X
+		LoremIpsumDummyText
+*/
+
+/* _________________________________________
+   Inicio cabecera */
+
+#include <stdio.h>
+
+void clearBuffer();
+
+/* _________________________________________
+   Inicio main() */
+
+int main()
+{
+	printf("\n_________________________________________START\n\n");
+    
+    
+    
+	printf("\n_________________________________________END\n\n");
+	return 0;
+}
+
+/* _________________________________________
+   Inicio definicion de funciones */
+
+void clearBuffer()
+{
+	while (getchar() != '\n');
+}
