fdb6723004189269e8dada8cb3f87110fc88d3e5
Stefan <trfstephan@gmail.com>
2025-01-29 16:55:23 +0100

    eliminar archivos de configuración de Visual Studio Code y archivo de prueba en C

--- pruebas/08_puntero/main.c
+++ pruebas/08_puntero/main.c
@@ -29,7 +29,31 @@ int main()
 {
 	printf("\n_________________________________________START\n\n");
 
+	char Nombre[3];
+	Nombre[0] = 'A';
+    Nombre[1] = 'n';
+	Nombre[2] = 'a';
+	printf("%s\n", Nombre);
 
+	char Nombre2[6];
+	Nombre2[0] = 'A';
+	Nombre2[1] = 'n';
+	Nombre2[2] = 'a';
+	Nombre2[3] = 'b';
+	Nombre2[4] = 'e';	
+	Nombre2[5] = 'l';
+	printf("%s\n", Nombre2);
+
+	// char* pNombre;
+	// *(pNombre ) = 'J';	
+	// *(pNombre + 1) = 'o';
+	// *(pNombre + 2) = 's';
+	// *(pNombre + 3) = 'e';
+	// *(pNombre + 4) = '\0';
+	// *(pNombre + 5) = 'L';
+	// *(pNombre + 6) = 'u';
+	// *(pNombre + 7) = 'i';	
+	// *(pNombre + 8) = 's';	
 
 	printf("\n_________________________________________END\n\n");
 	return 0;
