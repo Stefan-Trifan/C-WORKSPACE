4d95fdbf9e28ebc84323cc36110cd4e6d90fd91f
Stefan <trfstephan@gmail.com>
2025-01-30 18:21:28 +0100

    agregar implementación de arreglos bidimensionales y optimizar la impresión de arreglos unidimensionales

--- udemy/13_arreglos_con_listas_y_tablas/121_arrays_bidimensionales/main.c
+++ udemy/13_arreglos_con_listas_y_tablas/121_arrays_bidimensionales/main.c
@@ -19,6 +19,10 @@
    Inicio cabecera */
 
 #include <stdio.h>
+#define max_row 3
+#define max_col 3
+
+// tipoDeDato nombreDelArray [numFilas][numColumnas]
 
 void clearBuffer();
 
@@ -29,19 +33,40 @@ int main()
 {
 	printf("\n_________________________________________START\n\n");
     
-    int array[10];
+    int tabla[max_row][max_col] = {{1,2,3},{4,5,6},{7,8,9}};
 
-	for(int i = 0; i < 10; i++)
+	printf("Matriz inicial: \n");
+	for(int row = 0; row < max_row; row++)
 	{
-		printf("Introduce el valor para la posicion %d del array: ", i);
-		scanf("%d", &array[i]);
+		for(int col = 0; col < max_col; col++)
+		{
+			printf("%d  ", tabla[row][col]);
+		}
+		printf("\n");
 	}
 
-	printf("El array ha quedado de esta forma: ");
-	for(int i = 0; i < 10; i++)
+	printf("\n\nMatriz Ahora introduce los dats que tu quieras: \n");
+	for(int row = 0; row < max_row; row++)
 	{
-		printf("%i", array[i]);
+		for(int col = 0; col < max_col; col++)
+		{
+			printf("-> ");
+			scanf("%i", &tabla[row][col]);
+		}
 	}
+
+
+	printf("\n\nMatriz final: \n");
+	for(int row = 0; row < max_row; row++)
+	{
+		for(int col = 0; col < max_col; col++)
+		{
+			printf("%d  ", tabla[row][col]);
+		}
+		printf("\n");
+	}
+
+
     
 	printf("\n_________________________________________END\n\n");
 	return 0;
