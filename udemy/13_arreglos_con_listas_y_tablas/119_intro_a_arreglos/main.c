9380342924301e6f9fe77c2114dabffc6cc0e94f
Stefan <trfstephan@gmail.com>
2025-01-30 17:46:15 +0100

    agregar nuevos ejemplos de arreglos unidimensionales

--- udemy/13_arreglos_con_listas_y_tablas/119_intro_a_arreglos/main.c
+++ udemy/13_arreglos_con_listas_y_tablas/119_intro_a_arreglos/main.c
@@ -15,6 +15,8 @@
 		LoremIpsumDummyText
 */
 
+// TipoDeDato NombreDelArray[tamaño] = {valor1, valor2, ..., valorN};
+
 /* _________________________________________
    Inicio cabecera */
 
@@ -29,7 +31,16 @@ int main()
 {
 	printf("\n_________________________________________START\n\n");
     
-    
+    int array[3] = {24,5,26};
+
+	printf("Posicion 0: %i\n", array[0]);
+	printf("Posicion 1: %i\n", array[1]);
+	printf("Posicion 2: %i\n", array[2]);
+
+	array[0] = 4;
+	array[2] = 6;
+
+	printf("Los nuevos valores son: %i %i %i", array[0], array[1], array[2]);
     
 	printf("\n_________________________________________END\n\n");
 	return 0;
