7f02855e520c170beafc97e708cdcb4e50cbf11e
Stefan <trfstephan@gmail.com>
2025-02-01 23:50:30 +0100

    agregar implementación para ejercicios de matrices y funciones de entrada/salida

--- udemy/14_ejers_arrays/129_sacar_promedio/main.c
+++ udemy/14_ejers_arrays/129_sacar_promedio/main.c
@@ -19,7 +19,7 @@
    Inicio cabecera */
 
 #include <stdio.h>
-
+float pedirEntero();
 void clearBuffer();
 
 /* _________________________________________
@@ -29,7 +29,20 @@ int main()
 {
 	printf("\n_________________________________________START\n\n");
     
-    
+    float notas[8] = {0}, notasTotal = 0, promedio = 0;
+
+	printf("Introduce tus ocho notas\n");
+	for(int i = 0; i < 8; i++)
+	{
+		printf("Nota %d: ", i + 1);
+		notas[i] = pedirEntero();
+		notasTotal += notas[i];
+	}
+
+	promedio = notasTotal / 8;
+
+	printf("El promedio es: %.2f\n", promedio);
+
     
 	printf("\n_________________________________________END\n\n");
 	return 0;
@@ -37,6 +50,26 @@ int main()
 
 /* _________________________________________
    Inicio definicion de funciones */
+
+float pedirEntero()
+{
+	float num = 0, esValido = 1;
+	do
+	{
+		esValido = scanf("%f", &num);
+		clearBuffer();
+		if (esValido == 0)
+		{
+			printf(
+				"\033[1;31mERROR: El tipo de dato introducido no es válido. Por favor, inténtelo de nuevo. \n\033[0m"
+				"\033[1;31m-> \033[0m"
+			);
+			esValido = 0;
+		}
+	}
+	while (esValido != 1);
+	return num;
+}
 
 void clearBuffer()
 {
