517221fb48b34fb02804ef48e55491085d6e2e0c
Stefan <trfstephan@gmail.com>
2025-02-05 17:21:17 +0100

    agregar nuevas implementaciones para el cálculo de la media y manejo de argumentos en la línea de comandos

--- udemy/18_apuntadores/170_paso_parametros_por_arrays/main.c
+++ udemy/18_apuntadores/170_paso_parametros_por_arrays/main.c
@@ -19,7 +19,9 @@
    Inicio cabecera */
 
 #include <stdio.h>
+#define TAM_MAX 5
 
+void llenarArray(int Array[], int dimension);
 void clearBuffer();
 
 /* _________________________________________
@@ -29,7 +31,17 @@ int main()
 {
 	printf("\n_________________________________________START\n\n");
     
-    
+    int miArray[TAM_MAX];
+
+	llenarArray(miArray, TAM_MAX);
+
+	// Recuerda que por defecto el array se hace con paso de parametros por referencia
+	// esto nos permite editarlo desde otra funcion
+
+	for(int i = 0; i < TAM_MAX; i++)
+	{
+		printf("Posicion %d valor %d\n", i, miArray[i]);
+	}
     
 	printf("\n_________________________________________END\n\n");
 	return 0;
@@ -37,6 +49,19 @@ int main()
 
 /* _________________________________________
    Inicio definicion de funciones */
+
+/**
+ * Esta funcion recibe un Array y lo llena
+ * No se indica el numero de indices que recibira, se hace con otra variable
+ */
+void llenarArray(int Array[], int dimension)
+{
+	for(int i = 0; i < dimension; i++)
+	{
+		printf("Introduce la posicion %d el array: ", i);
+		scanf("%d", &Array[i]);
+	}
+}
 
 void clearBuffer()
 {
