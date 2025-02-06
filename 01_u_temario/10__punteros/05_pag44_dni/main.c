fac01988d95e43ff1a67e9fd0ae96a43ea943dc9
Stefan <trfstephan@gmail.com>
2025-02-06 09:06:44 +0100

    agregar nueva implementación de main.c en 05_pag44_dni, eliminando el archivo obsoleto en 05_ y configurando ajustes de VSCode

--- 01_u_temario/10__punteros/05_pag44_dni/main.c
+++ 01_u_temario/10__punteros/05_pag44_dni/main.c
@@ -13,11 +13,14 @@
 
 	* 	Enunciado X
 		LoremIpsum
+
+		
 */
 
 /* _________________________________________
    Inicio cabecera */
 
+#include <stdlib.h>
 #include <stdio.h>
 
 void clearBuffer();
@@ -28,9 +31,29 @@ void clearBuffer();
 int main(int argc, char *argv[])
 {
 	printf("\n_________________________________________START\n\n");
-    
-    
-    
+
+	if(argc != 2)
+	{
+		printf("Ha ocurrido un error. Por favor, inténtelo de nuevo.\n\n\n");
+		return 1;
+	}
+
+	int numeroDni = 0, resto;
+	char letrasDNI[23] = 
+	{
+		'T','R','W','A','G',
+		'M','Y','F','P','D',
+		'X','B','N','J','Z',
+		'S','Q','V','H','L',
+		'C','K','E'
+	};
+
+	numeroDni = strtol(argv[1], NULL, 10);
+	resto = numeroDni % 23;
+	char miLetra = letrasDNI[resto];
+	
+	printf("%d%c\n", numeroDni, miLetra);
+
 	printf("\n_________________________________________END\n\n");
 	return 0;
 }
