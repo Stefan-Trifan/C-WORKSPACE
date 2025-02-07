1ba360755129e35de50e883e41ecec86d8e6a33d
Stefan <trfstephan@gmail.com>
2025-02-07 22:58:25 +0100

    agregar función para copiar arreglos en main.c y mejorar la visualización de datos

--- udemy/14_ejers_arrays/130_copiar_array/main.c
+++ udemy/14_ejers_arrays/130_copiar_array/main.c
@@ -19,6 +19,8 @@
    Inicio cabecera */
 
 #include <stdio.h>
+#define TAM_ARR 5
+void copiarArray(int *arr1,int *arr2);
 
 void clearBuffer();
 
@@ -29,7 +31,22 @@ int main(int argc, char *argv[])
 {
 	printf("\n_________________________________________START\n\n");
     
-    
+	int arr1[TAM_ARR] = {1,2,3,4,5},
+	    arr2[TAM_ARR] = {0};
+
+	printf("Al principio\n");
+	for(int i = 0; i < TAM_ARR; i++)
+		printf("arr1[i] = %d\n", arr1[i]);
+	printf("\n");
+	for(int i = 0; i < TAM_ARR; i++)
+		printf("arr2[i] = %d\n", arr2[i]);
+	printf("\n");
+
+	// Copiamos
+	copiarArray(arr1, arr2);
+
+	for(int i = 0; i < TAM_ARR; i++)
+		printf("arr2[i] = %d\n", arr2[i]);
     
 	printf("\n_________________________________________END\n\n");
 	return 0;
@@ -37,6 +54,13 @@ int main(int argc, char *argv[])
 
 /* _________________________________________
    Inicio definicion de funciones */
+
+void copiarArray(int *arr1,int *arr2)
+{
+	printf("Copiando el array...\n\n");
+	for(int i = 0; i < TAM_ARR; i++)
+	arr2[i] = arr1[i];
+}
 
 void clearBuffer()
 {
