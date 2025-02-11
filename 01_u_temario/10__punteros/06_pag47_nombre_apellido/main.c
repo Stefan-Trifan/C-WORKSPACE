cc42d31558e216579f1dcbd03f7d369132df66ef
Stefan <trfstephan@gmail.com>
2025-02-11 11:50:39 +0100

    ejercicio estructura

--- 01_u_temario/10__punteros/06_pag47_nombre_apellido/main.c
+++ 01_u_temario/10__punteros/06_pag47_nombre_apellido/main.c
@@ -28,15 +28,52 @@ void clearBuffer();
 int main(int argc, char *argv[])
 {
 	printf("\n_________________________________________START\n\n");
+
+	char nombre[100];      // Para almacenar el nombre
+	char apellidos[100];   // Para almacenar los apellidos
+	int coma_encontrada = 0; // Bandera para detectar la coma
+	int index_nombre = 0, index_apellido = 0; // Índices para construir cadenas	
     
-	char nombre[50], apellido1[50], apellido2[50];
-	int  cont = 0;
-
-
-	for(int i = 1; i < 20; i++)
-	{
-		
+	// Verificar que al menos hay dos argumentos (apellido y nombre)
+	if (argc < 2) {
+		printf("Uso: %s <primer apellido> <segundo apellido>, <nombre>\n", argv[0]);
+		return 1; // Código de error
 	}
+
+	// Recorrer los argumentos desde el segundo (argv[1] en adelante)
+	for (int i = 1; i < argc; i++) {
+		// Recorrer cada carácter del argumento actual
+		for (int j = 0; argv[i][j] != '\0'; j++) {
+			if (argv[i][j] == ',') {
+				coma_encontrada = 1; // Encontramos la coma
+				continue; // Saltamos la coma sin copiarla
+			}
+
+			if (coma_encontrada) {
+				// Copiar caracteres al nombre
+				nombre[index_nombre++] = argv[i][j];
+			} else {
+				// Copiar caracteres al apellido
+				apellidos[index_apellido++] = argv[i][j];
+			}
+		}
+		
+		// Agregar un espacio entre palabras, excepto después de la última
+		if (i < argc - 1) {
+			if (coma_encontrada) {
+				nombre[index_nombre++] = ' ';
+			} else {
+				apellidos[index_apellido++] = ' ';
+			}
+		}
+	}
+
+	// Agregar terminador de cadena
+	nombre[index_nombre] = '\0';
+	apellidos[index_apellido] = '\0';
+
+	// Mostrar el resultado formateado
+	printf("%s %s\n", nombre, apellidos);
     
 	printf("\n_________________________________________END\n\n");
 	return 0;
--- 01_u_temario/20_estructuras/01_pag5_struct_persona/main.c
+++ 01_u_temario/20_estructuras/01_pag5_struct_persona/main.c
@@ -20,6 +20,13 @@
 
 #include <stdio.h>
 
+struct Persona
+{
+	char nombre[20];
+	float edad;
+	float altura;
+};
+
 void clearBuffer();
 
 /* _________________________________________
@@ -29,6 +36,15 @@ int main(int argc, char *argv[])
 {
 	printf("\n_________________________________________START\n\n");
     
+	struct Persona persona1;
+
+	persona1.nombre[20] = " ";
+	persona1.edad = 27;
+	persona1.altura = 186;
+
+	printf("Nombre: %s\n", persona1.nombre);
+	printf("Edad: %.2f\n", persona1.edad);
+	printf("Altura: %.2f cm\n", persona1.altura);
     
     
 	printf("\n_________________________________________END\n\n");
