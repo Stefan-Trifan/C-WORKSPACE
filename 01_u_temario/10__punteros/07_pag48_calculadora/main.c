8abc761e9409577a58931532d5099968d8e10556
Stefan <trfstephan@gmail.com>
2025-02-12 16:58:09 +0100

    eliminar archivo main.c obsoleto y agregar nueva implementación de calculadora

--- 01_u_temario/10__punteros/07_pag48_calculadora/main.c
+++ 01_u_temario/10__punteros/07_pag48_calculadora/main.c
@@ -29,7 +29,56 @@ int main(int argc, char *argv[])
 {
 	printf("\n_________________________________________START\n\n");
     
-    
+    float op1 = 0.0, op2 = 0.0;
+
+	int posicon_igual = 0;
+	int letra = 0;
+
+	char ope[14] = "";
+	char temp = ' ';
+
+	for(int argumento = 1; argumento < argc; argumento++)
+	{
+
+		do
+		{
+			printf("%c", argv[argumento][letra]);
+			letra++;
+		} 
+		while (argv[argumento][letra] != '\0');
+
+		letra = 0;
+		printf(" ");
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// TODO Hacer validacion para comprobar que el usuario ha introducido 4 parametros
+
+	// TODO Leer Operando (valido 1 y 2)
+		// TODO Comprobar Inicio
+		// TODO Si Correcto -> Asignar Operando
+
+	// TODO Leer Operación
+		// TODO Comprobar Inicio
+		// TODO Si Correcto -> Asignar Operacion
+
+	// TODO Realizar Operaciónote
     
 	printf("\n_________________________________________END\n\n");
 	return 0;
