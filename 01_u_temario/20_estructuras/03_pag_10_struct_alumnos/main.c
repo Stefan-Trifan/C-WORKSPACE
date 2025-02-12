f8b086be0325aa77bced13f4f5ba8706623b6bcf
Stefan <trfstephan@gmail.com>
2025-02-12 10:17:00 +0100

    eliminar archivos obsoletos

--- 01_u_temario/20_estructuras/03_pag_10_struct_alumnos/main.c
+++ 01_u_temario/20_estructuras/03_pag_10_struct_alumnos/main.c
@@ -19,6 +19,17 @@
    Inicio cabecera */
 
 #include <stdio.h>
+#include <string.h>
+
+#define TAM 50
+
+struct alumnos_t 
+{
+	int edad;
+	float altura;
+	char nombre[TAM];
+	char apellido[TAM];
+};
 
 void clearBuffer();
 
@@ -28,8 +39,21 @@ void clearBuffer();
 int main(int argc, char *argv[])
 {
 	printf("\n_________________________________________START\n\n");
-    
-    
+
+	struct alumnos_t nuevo_alumno[TAM];
+
+	nuevo_alumno[0].edad = 20;
+	nuevo_alumno[0].altura = 1.80;
+	strcpy(nuevo_alumno[0].nombre, "Santiago");
+	strcpy(nuevo_alumno[0].apellido, "Castillo");
+
+
+	printf("Edad:     %d\n", nuevo_alumno[0].edad);
+	printf("Altura:   %.2f\n", nuevo_alumno[0].altura);
+	printf("Nombre:   %s\n", nuevo_alumno[0].nombre);
+	printf("Apellido: %s\n", nuevo_alumno[0].apellido);
+
+
     
 	printf("\n_________________________________________END\n\n");
 	return 0;
--- pruebas/13_/main.c
+++ pruebas/13_/main.c
@@ -1,26 +1,4 @@
-/*
-	*	Autor
-		Stefan Trifan
-
-	* 	Compilar
-		mac     : comp (compilar)
-				  run  (ejecutar)
-				  brun (compilar y ejecutar)
-		windows : gcc main.c -o main.exe
-				: ./main.exe
-
-	*   Estado: 
-
-	* 	Enunciado X
-		LoremIpsum
-*/
-
-/* _________________________________________
-   Inicio cabecera */
-
 #include <stdio.h>
-
-void clearBuffer();
 
 /* _________________________________________
    Inicio main() */
@@ -29,16 +7,9 @@ int main(int argc, char *argv[])
 {
 	printf("\n_________________________________________START\n\n");
     
-    
+    short a = 18;
+	short b = 65000;
     
 	printf("\n_________________________________________END\n\n");
 	return 0;
-}
-
-/* _________________________________________
-   Inicio definicion de funciones */
-
-void clearBuffer()
-{
-	while (getchar() != '\n');
 }
