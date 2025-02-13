9d19fa4a70567d0dc6b35d70c4458280eded0fa7
Stefan <trfstephan@gmail.com>
2025-02-13 21:15:59 +0100

    agregar nueva implementación de la funcionalidad para invertir un array de caracteres

--- udemy/14_ejers_arrays/139_cantidad_caracteres/main.c
+++ udemy/14_ejers_arrays/139_cantidad_caracteres/main.c
@@ -19,7 +19,7 @@
    Inicio cabecera */
 
 #include <stdio.h>
-
+#define TAM 30
 void clearBuffer();
 
 /* _________________________________________
@@ -29,7 +29,32 @@ int main(int argc, char *argv[])
 {
 	printf("\n_________________________________________START\n\n");
     
-    
+    char arr_normal[TAM] = {0}, arr_reves[TAM] = {0};
+	int numLetras = 0;
+
+	printf("Introduce el array incial: ");
+	for(int i = 0; i < TAM - 1; i++)
+	{
+		arr_normal[i] = getchar();
+		numLetras++;
+		if(arr_normal[i] == '\n' )
+		{
+			numLetras--;
+			arr_normal[i] = '\0';
+			break;
+		}
+	}
+
+	for(int i = 0; i < numLetras; i++)
+	{
+		for(int j = numLetras; j >= 0; j--)
+		{
+			arr_reves[i] = arr_normal[j];
+		}
+	}
+
+	printf("Introduce el array final es: ");
+	printf("%s\n", arr_reves);
     
 	printf("\n_________________________________________END\n\n");
 	return 0;
