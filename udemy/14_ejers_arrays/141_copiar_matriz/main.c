405baa602a4bc7c1f2764189dc8ed7cbbfb553cd
Stefan <trfstephan@gmail.com>
2025-02-13 21:45:29 +0100

    agregar nueva implementación en 141_copiar_matriz

---
+++ udemy/14_ejers_arrays/141_copiar_matriz/main.c
@@ -0,0 +1,89 @@
+/*
+	*	Autor
+		Stefan Trifan
+
+	* 	Compilar
+		mac     : comp (compilar)
+				  run  (ejecutar)
+				  brun (compilar y ejecutar)
+		windows : gcc main.c -o main.exe
+				: ./main.exe
+
+	*   Estado: 
+
+	* 	Enunciado X
+		LoremIpsum
+*/
+
+/* _________________________________________
+   Inicio cabecera */
+
+#include <stdio.h>
+#define ROW 2
+#define COL 2
+int pedirEntero();
+void clearBuffer();
+
+/* _________________________________________
+   Inicio main() */
+
+int main(int argc, char *argv[])
+{
+	printf("\n_________________________________________START\n\n");
+    
+    int mtx1[ROW][COL] = {0}, mtx2[ROW][COL] = {0};
+
+	printf("mtx1: \n");
+	for(int i = 0; i < ROW; i++)
+	{
+		for(int j = 0; j < COL; j++)
+		{
+			printf("mtx1[%d][%d]: ", i, j);
+			mtx1[i][j] = pedirEntero();
+			mtx2[i][j] = mtx1[i][j];
+		}
+	}
+	printf("\n");
+
+	printf("mtx2: \n");
+	for(int i = 0; i < ROW; i++)
+	{
+		for(int j = 0; j < COL; j++)
+		{
+			printf("%2d ", mtx2[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("\n_________________________________________END\n\n");
+	return 0;
+}
+
+/* _________________________________________
+   Inicio definicion de funciones */
+
+int pedirEntero()
+{
+	int num = 0, esValido = 1;
+	do
+	{
+		esValido = scanf("%d", &num);
+		clearBuffer();
+		if (esValido == 0)
+		{
+			printf(
+				"\033[1;31mERROR: El tipo de dato introducido no es válido. "
+				"Por favor, inténtelo de nuevo. \n\033[0m" 
+				"\033[1;31m-> \033[0m"
+			);
+			esValido = 0;
+		}
+	}
+	while (esValido != 1);
+	return num;
+}
+
+void clearBuffer()
+{
+	while (getchar() != '\n');
+}
