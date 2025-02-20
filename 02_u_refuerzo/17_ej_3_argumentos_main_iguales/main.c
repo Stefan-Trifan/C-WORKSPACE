d3aae17ec469660a3dd1120823862a53fcb8a1f7
Stefan <trfstephan@gmail.com>
2025-02-20 09:25:24 +0100

    agregar nueva implementación en main.c para el ejercicio de cadenas

--- 02_u_refuerzo/17_ej_3_argumentos_main_iguales/main.c
+++ 02_u_refuerzo/17_ej_3_argumentos_main_iguales/main.c
@@ -5,7 +5,8 @@
     *   Estado
 
     *   Enunciado X
-        Escribe un programa en lenguaje C al que se le pase por argumentos del main dos cadenas (tamaño máximo 20 caracteres) y que compruebe que son iguales.
+        Escribe un programa en lenguaje C al que se le pase por argumentos del main 
+        dos cadenas (tamaño máximo 20 caracteres) y que compruebe que son iguales.
         El programa mostrará imprimirá por pantalla si son iguales o no después de comprobarlo
 */
 
@@ -15,6 +16,8 @@
 #include <stdio.h>
 
 // Funciones del programa
+int cadenasIguales(char* cadena1, char* cadena2);
+int tamCadena(char* cadena);
 
 // Funciones auxiliares
 void clearBuffer();
@@ -36,11 +39,19 @@ int main(int argc, char *argv[])
    Inicio definicion de funciones */
 
 // Funciones del programa
+/**
+ * Función que se le pasa como parámetros dos cadenas y 
+ * devuelve verdadero si son iguales y falso si no lo son
+ */
 int cadenasIguales(char* cadena1, char* cadena2)
 {
 
 }
 
+/**
+ * Función que se le pasa como parámetro una cadena y 
+ * devuelve el numero de caracteres que tiene
+ */
 int tamCadena(char* cadena)
 {
 
