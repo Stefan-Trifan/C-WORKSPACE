cb5e1c2a1753926b425a6c4c33de363398058b9c
Stefan <trfstephan@gmail.com>
2025-02-22 17:53:33 +0100

    agregar nuevas implementaciones de main en ejercicios con estructuras

--- udemy/16_estructuras/154_ejercicio_2/main.c
+++ udemy/16_estructuras/154_ejercicio_2/main.c
@@ -12,7 +12,18 @@
    Inicio cabecera */
 
 #include <stdio.h>
+#include <string.h>
+#define TAM 4
 
+typedef struct asignatura_t
+{
+	char materia[30];
+}asignatura_t;
+
+typedef struct alumno_t
+{
+	asignatura_t asignaturas[5];
+}alumno_t;
 // Funciones del programa
 
 // Funciones auxiliares
@@ -24,8 +35,15 @@ void clearBuffer();
 int main(int argc, char *argv[])
 {
 	printf("\n_________________________________________START\n\n");
-    
-    
+
+	alumno_t Jorge, Juan;
+	strcpy(Jorge.asignaturas[0].materia, "Matematicas");
+	strcpy(Jorge.asignaturas[1].materia, "Fisica");
+	strcpy(Jorge.asignaturas[2].materia, "Quimica");
+
+	strcpy(Juan.asignaturas[0].materia, "Musica");
+	strcpy(Juan.asignaturas[1].materia, "Tecnologia");
+	strcpy(Juan.asignaturas[2].materia, "Religion");
     
 	printf("\n_________________________________________END\n\n");
 	return 0;
