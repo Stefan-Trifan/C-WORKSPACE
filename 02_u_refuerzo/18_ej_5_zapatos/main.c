05ef91d884269ff963b37ce0e8d6d730c0e5f358
Stefan <trfstephan@gmail.com>
2025-02-24 21:18:02 +0100

    eliminar implementación antigua de pedir cadena y agregar nueva versión avanzada

--- 02_u_refuerzo/18_ej_5_zapatos/main.c
+++ 02_u_refuerzo/18_ej_5_zapatos/main.c
@@ -1,10 +1,10 @@
 /*
-    *  Autor
+    *   Autor
         Stefan Trifan
 
     *  Estado
 
-    *  Enunciado X
+    *   Enunciado X
         Lorem Ipsum
 */
 
@@ -13,11 +13,14 @@
 
 #include <stdio.h>
 #include <string.h>
+
 #define RED_BOLD "\033[1;31m"
 #define GREEN_BOLD "\033[1;32m"
 #define YELLOW_BOLD "\033[1;33m"
 #define RESET "\033[0m"
-#define TAM_STRING 5
+#define TAM_STRUCT 9
+#define TAM_STRING 4
+
 typedef struct zapato_t
 {
     int   referencia;
@@ -27,15 +30,18 @@ typedef struct zapato_t
     float precio;
     int   stock;
 }zapato_t;
+
 // Funciones del programa
 void introducirZapato(struct zapato_t *zapato, int *contZapatos);
-void imprimirZapato(struct zapato_t zapato);
+void imprimirZapatos(struct zapato_t *zapato, int contZapatos);
 void venderZapato(struct zapato_t *zapato, int *contZapatos);
 void imprimirMenu();
+
 // Funciones auxiliares
 int pedirEnteroPositivo();
 int pedirOpcionMenu();
 void pedirCadena(char *miembro, int tam);
+float pedirFloatPositivo();
 void clearBuffer();
 
 /* _________________________________________
@@ -45,22 +51,28 @@ int main(int argc, char *argv[])
 {
     printf("\n_________________________________________START\n\n");
 
-    zapato_t zapato[9]; // Maximo 10 zapatos
-    int opcion = 0, contZapatos = 0;
+    // Maximo 10 zapatos. Simulamos un stock inicial
+    zapato_t mi_zapato[TAM_STRUCT];
+    // = {
+    //     {1, "Verde", "Bota", 43, 79, 20},
+    //     {2, "Rojo", "Tacon", 42, 150, 10},
+    //     {3, "Amarillo", "Zapato", 43, 200, 5}
+    // }; 
+    int opcion = 0, mi_cont_zapatos = 0;
     
     do
     {
         printf(
-            // "+---------------------------------+\n"
+            "+---------------------------------+\n"
             "|              MENÚ               |\n"
-            // "|---------------------------------|\n"
-            // "|      Seleccione una opcion      |\n"
-            // "|                                 |\n"
+            "|---------------------------------|\n"
+            "|      Seleccione una opcion      |\n"
+            "|                                 |\n"
             "| [1] Introducir Zapato           |\n"
             "| [2] Imprimir Zapatos por talla  |\n"
             "| [3] Vender zapato               |\n"
             "| [4] Salir                       |\n"
-            // "+---------------------------------+\n"
+            "+---------------------------------+\n"
             "-> ");
         
         opcion = pedirOpcionMenu();
@@ -68,13 +80,13 @@ int main(int argc, char *argv[])
         switch (opcion)
         {
             case 1: 
-                introducirZapato(zapato, &contZapatos);
+                introducirZapato(mi_zapato, &mi_cont_zapatos);
                 break;
             case 2: 
-                imprimirZapato(zapato[contZapatos]);
+                imprimirZapatos(mi_zapato, mi_cont_zapatos);
                 break;
             case 3: 
-                venderZapato(zapato, &contZapatos);
+                venderZapato(mi_zapato, &mi_cont_zapatos);
                 break;
             case 4: 
                 printf("Gracias por utilizar el programa! \n");
@@ -91,110 +103,131 @@ int main(int argc, char *argv[])
 /* _________________________________________
    Inicio definicion de funciones */
 
-// typedef struct zapato_t
-// {
-//     int   referencia;
-//     char  color[TAM_STRING];
-//     char  tipo[TAM_STRING];
-//     int   talla;
-//     float precio;
-//     int   stock;
-// }zapato_t;
-
 // Funciones del programa
 void introducirZapato(struct zapato_t *zapato, int *contZapatos)
 {
-    printf("\nIntroduce los detalles de la nueva zapatilla \n\n");
+    printf("\nMENU / Introducir Zapato \n");
+    printf("Introduce los detalles de la nueva zapatilla \n\n");
 
-    // todo descomentar
- /*    printf("1. Referencia\n-> ");
-    zapato->referencia = pedirEnteroPositivo();
-    printf("DEBUG Has introducido referencia: %d\n", zapato->referencia); */
+    printf("1. Referencia \n-> ");
+    (zapato + *contZapatos)->referencia = pedirEnteroPositivo();
+    printf("DEBUG Has introducido referencia: %d\n\n", (zapato + *contZapatos)->referencia);
 
     printf("2. Color (máx %d caracteres)\n-> ", TAM_STRING - 1);
-    pedirCadena(zapato->color, TAM_STRING);
-    printf("DEBUG Has introducido color: %s\n", zapato->color);
+    pedirCadena((zapato + *contZapatos)->color, TAM_STRING);
+    printf("DEBUG Has introducido color: %s\n\n", (zapato + *contZapatos)->color);
 
-    printf("\n\n\n\n\n\n");
-    // todo descomentar
-/*     printf("3. Tipo\n-> ");
-    printf("\n");
+    printf("3. Tipo (máx %d caracteres)\n-> ", TAM_STRING - 1);
+    pedirCadena((zapato + *contZapatos)->tipo, TAM_STRING - 1);
+    printf("DEBUG Has introducido tipo: %s\n\n", (zapato + *contZapatos)->tipo);
 
     printf("4. Talla\n-> ");
-    printf("\n");
+    (zapato + *contZapatos)->talla = pedirEnteroPositivo();
+    printf("DEBUG Has introducido talla: %d\n\n", (zapato + *contZapatos)->talla);
 
     printf("5. Precio\n-> ");
-    printf("\n");
+    (zapato + *contZapatos)->precio = pedirFloatPositivo();
+    printf("DEBUG Has introducido precio: %.2f\n\n", (zapato + *contZapatos)->precio);
 
     printf("6. Stock\n-> ");
-    printf("\n");
+    (zapato + *contZapatos)->stock = pedirEnteroPositivo();
+    printf("DEBUG Has introducido stock: %d\n\n", (zapato + *contZapatos)->stock);
 
     (*contZapatos)++;
     printf(GREEN_BOLD
         "Nuevo zapato introducido \n"RESET
-        "Numero de zapatos: %d\n"); */
-    printf("\n");
+        "Numero de zapatos: %d\n", *contZapatos);
+    printf("\n\n");
 }
 
-void imprimirZapato(struct zapato_t zapato)
+void imprimirZapatos(struct zapato_t *zapato, int contZapatos)
 {
+    int talla_seleccionada = 0, contadorCoincidencias = 0;
+    printf("\nMENU / Imprimir Zapatos por talla \n");
 
+    printf("Que talla deseas buscar?\n-> ");
+    talla_seleccionada = pedirEnteroPositivo();
+
+    for(int i = 0; i < contZapatos; i++)
+    {
+        if(zapato[i].talla == talla_seleccionada)
+        {
+            printf(GREEN_BOLD
+                "\n"
+                "Ref    : %d\n"
+                "Color  : %s\n"
+                "Tipo   : %s\n"
+                "Talla  : %d\n"
+                "Precio : %.2f\n"
+                "Stock  : %d\n"
+                "\n"RESET, 
+                zapato[i].referencia, 
+                zapato[i].color, 
+                zapato[i].tipo, 
+                zapato[i].talla, 
+                zapato[i].precio, 
+                zapato[i].stock
+            );
+            contadorCoincidencias++;
+        }
+    }
+    if(contadorCoincidencias == 0)
+    {
+        printf(YELLOW_BOLD"\nNo se han zapato con la talla %d\n"RESET, talla_seleccionada);
+    }
+    else
+    {
+        printf(GREEN_BOLD"Se han encontrado %d zapato(s) con talla %d\n"RESET, contadorCoincidencias, talla_seleccionada);
+    }
+    printf("\n\n");
 }
 
 void venderZapato(struct zapato_t *zapato, int *contZapatos)
 {
+    int refZapatoVendido, numZapatosVendidos = 0;
+    printf("\nMENU / Vender zapato \n");
+    for(int i = 0; i < *contZapatos; i++)
+    {
+        printf(GREEN_BOLD
+            "\n"
+            "Ref    : %d\n"
+            "Color  : %s\n"
+            "Tipo   : %s\n"
+            "Talla  : %d\n"
+            "Precio : %.2f\n"
+            "Stock  : %d\n"
+            "\n"RESET, 
+            zapato[i].referencia, 
+            zapato[i].color, 
+            zapato[i].tipo, 
+            zapato[i].talla, 
+            zapato[i].precio, 
+            zapato[i].stock
+        );
+    }
 
+    printf("Selecciona la referencia del zapato que desea vender\n-> ");
+    refZapatoVendido = pedirEnteroPositivo();
+
+    for(int i = 0; i < *contZapatos; i++)
+    {
+        if((zapato + i)->referencia == refZapatoVendido && (zapato + i)->stock > 0)
+        {
+            (zapato + i)->stock--;
+            numZapatosVendidos++;
+        }
+    }
+
+    if(numZapatosVendidos == 0)
+    {
+        printf("No se han encontrado zapatos\n");
+    }
+
+    printf("\n\n");
 }
 
-
-/**
- * 
- * rojo\n
- * 
- */
 
 // Funciones auxiliares
-void pedirCadena(char *miembro, int tam)
-{
-    int i = 0, esValido = 0;
-    char c;
-    do
-    {
-        i = 0;
-        while (i < tam - 1)
-        {
-            c = getchar();
-            
-            // printf("DEBUG: i = %d, c = %c (%d)\n", i, c, c);
-            if(c == '\n')
-                break;
-
-            miembro[i] = c;
-            i++; 
-        }
-        miembro[i] = '\0'; 
-
-        if(i == tam - 1)
-            c = getchar();
-        // printf("DEBUG SALIDA WHILE: i = %d, c = %c (%d)\n", i, c, c);
-        
-        if(i == tam - 1 && c != '\n')
-        {
-            printf(YELLOW_BOLD
-                "Has introducido demasiados caracteres\n"
-                "Intentalo de nuevo\n"
-                "-> "RESET);
-            clearBuffer();
-            esValido = 0;
-        }
-        else
-            esValido = 1;
-    } 
-    while (esValido != 1);
-}
-
-
-
 int pedirEnteroPositivo()
 {
     int num = 0, esValido = 0;
@@ -221,6 +254,7 @@ int pedirEnteroPositivo()
     while (esValido != 1);
     return num;
 }
+
 int pedirOpcionMenu()
 {
     int num = 0, esValido = 0;
@@ -241,11 +275,74 @@ int pedirOpcionMenu()
     while (esValido != 1);
     return num;
 }
-// void pedirCadena(char *miembro, int tam)
-// {
-//     fgets(miembro, tam, stdin);
-//     miembro[strcspn(miembro, "\n")] = '\0'; // Eliminar el salto de línea
-// }
+
+void pedirCadena(char *miembro, int tam)
+{
+    int i = 0, esValido = 0;
+    char c;
+    do
+    {
+        i = 0;
+        while (i < tam - 1)
+        {
+            c = getchar();
+
+            // printf("DEBUG: i = %d, c = %c (%d)\n", i, c, c);
+            if(c == '\n')
+                break;
+
+            miembro[i] = c;
+            i++; 
+        }
+        miembro[i] = '\0'; 
+
+        if(i == tam - 1)
+        {
+            c = getchar();
+        }
+        // printf("DEBUG SALIDA WHILE: i = %d, c = %c (%d)\n", i, c, c);
+        
+        if(i == tam - 1 && c != '\n')
+        {
+            printf(YELLOW_BOLD
+                "Has introducido demasiados caracteres\n"
+                "Intentalo de nuevo\n"
+                "-> "RESET);
+            clearBuffer();
+            esValido = 0;
+        }
+        else
+            esValido = 1;
+    } 
+    while (esValido != 1);
+}
+float pedirFloatPositivo()
+{
+    float num = 0;
+    int esValido = 0;
+    do
+    {
+        esValido = scanf("%f", &num);
+        clearBuffer();
+        if(esValido != 1)
+        {
+            printf(
+                "\033[1;31mERROR: El tipo de dato introducido no es válido.\n"
+                "-> \033[0m"
+            );
+        }
+        if(num < 0 && esValido == 1)
+        {
+            printf(
+                "\033[1;31mERROR: Selecciona un una opcion el 1 al 4\n"
+                "-> \033[0m"
+            );
+            esValido = 0;
+        }
+    } 
+    while (esValido != 1);
+    return num;
+}
 void clearBuffer() {
     while (getchar() != '\n');
 }
--- libreria/pedir_cadena_avanzado/main.c
+++ libreria/pedir_cadena_avanzado/main.c
@@ -12,9 +12,9 @@
    Inicio cabecera */
 
 #include <stdio.h>
+
 #define TAM 5
-#define YELLOW_BOLD "\033[1;33m"
-#define RESET "\033[0m"
+
 // Funciones del programa
 
 // Funciones auxiliares
@@ -40,54 +40,54 @@ int main(int argc, char *argv[])
 
 /* _________________________________________
    Inicio definicion de funciones */
-// tam     :      5
-// Entrada : rojo\n
-// char c  : rojo\n  
-// text[i] : rojo\0
-// i       : 4
 
 // Funciones del programa
+
+// Funciones auxiliares
 void pedirCadena(char *text, int tam)
 {
-	int esValido = 0, i = 0;
-	char c;
-	do
-	{
-		i = 0;
-		while(i < tam - 1)
-		{
-			c = getchar();
-			// printf("DEBUG: i = %d, c = %c (%d)\n", i, c, c);
-			if(c == '\n')
+    int i = 0, esValido = 0;
+    char c;
+    do
+    {
+        i = 0;
+        while (i < tam - 1)
+        {
+            c = getchar();
+
+            // printf("DEBUG: i = %d, c = %c (%d)\n", i, c, c);
+            if(c == '\n')
 			{
 				break;
 			}
-			text[i] = c;
-			i++;
-		}
-		text[i] = '\0';
-		
-		c = getchar();
-		// printf("DEBUG SALIDA WHILE: i = %d, c = %c (%d)\n", i, c, c);
+                
+            text[i] = c;
+            i++; 
+        }
+        text[i] = '\0'; 
 
-		if(i == tam - 1 && c != '\n')
-		{
-			printf(
+        if(i == tam - 1)
+        {
+            c = getchar();
+        }
+        // printf("DEBUG SALIDA WHILE: i = %d, c = %c (%d)\n", i, c, c);
+        
+        if(i == tam - 1 && c != '\n')
+        {
+            printf(
                 "\033[1;33mHas introducido demasiados caracteres\n"
                 "Intentalo de nuevo\n"
                 "-> \033[0m");
             clearBuffer();
-			esValido = 0;
-		}
-		else
+            esValido = 0;
+        }
+        else
 		{
 			esValido = 1;
 		}
-	} 
-	while (esValido != 1);
+    } 
+    while (esValido != 1);
 }
-
-// Funciones auxiliares
 void clearBuffer()
 {
 	while (getchar() != '\n');
