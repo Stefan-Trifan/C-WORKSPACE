42e39e890671ef9dc2881620ac793ade685978fa
Stefan <trfstephan@gmail.com>
2025-03-01 12:08:09 +0100

    mejorar la validación de entrada en la función main y agregar función verUso para mostrar instrucciones

--- 03_u_evaluables/Stefan_Trifan_Practica1_IPR1/3_lista_objetos/main.c
+++ 03_u_evaluables/Stefan_Trifan_Practica1_IPR1/3_lista_objetos/main.c
@@ -35,6 +35,7 @@ objeto_t;
 int procesarArgumentos(int argc);
 int comprobarObjetos();
 void imprimirObjetos(char letra);
+void verUso();
 // Funciones auxiliares
 void clearBuffer();
 
@@ -52,33 +53,27 @@ int main(int argc, char *argv[])
     // Comprobamos que el usuario respeta el limite de objetos
     if(argc < 2 || argc > 11)
     {
-        printf(RED
-            "ERROR  : Tienes que introducir entre 1 y 10 objetos.\n"YELLOW
-            "Uso    : ./main [ID]:[PESO]:[CATEG]...[ID]:[PESO]:[CATEG]\n"
-            "Ejemplo: ./main 101:2.5:A 104:4.0:B\n"RED
-            "\n_________________________________________FAIL\n\n"RESET);
+        printf(RED"ERROR: Tienes que introducir entre 1 y 10 objetos.\n"RESET);
+        verUso();
         return 1; // error
     }
     num_objetos = argc - 1;
 
     // Comprobamos que el formato de los objetos es correcto 
-    // ./main 101:2.5:A\0 102:3.1:B\0 103:1.8:C\0 104:4.0:A\0 105:2.2:B\0 106:3.9:C\0
     for(int i = 1; i < argc; i++)
     {
         int j                = 0,
             cont_separadores = 0,
             cont_punto_float = 0;
-            
+        
+        // ID (INT)
         while(argv[i][j] != ':')
         {
             // Comprobamos si el ID  son solo numeros
             if(!(argv[i][j] >= '0' && argv[i][j] <= '9'))
             {
-                printf(RED
-                    "ERROR 1: El ID solo puede contener numeros\n"YELLOW
-                    "Uso    : ./main [ID]:[PESO]:[CATEG]...[ID]:[PESO]:[CATEG]\n"
-                    "Ejemplo: ./main 101:2.5:A 104:4.0:B\n"RED
-                    "\n_________________________________________FAIL\n\n"RESET);
+                printf(RED"ERROR: El ID solo puede contener numeros\n"RESET);
+                verUso();
                 return 1;
             } 
             j++;
@@ -87,42 +82,57 @@ int main(int argc, char *argv[])
         {
             cont_separadores++;
             j++;
+        }
+         // PESO (FLOAT)
+        if(argv[i][j] == '.')
+        {
+            printf(RED"ERROR: El punto del float esta mal colocado. Falta parte entera\n"RESET);
+            verUso();
+            return 1;
         }
         while(argv[i][j] != ':')
         {
-            // TODO Comprobamos el float
-            if
-            (
-                !(argv[i][j] >= '0' && argv[i][j] <= '9')
-            )
+            // Comprobamos el float es compuesto solo por digitos o punto
+            if(!((argv[i][j] >= '0' && argv[i][j] <= '9') || argv[i][j] == '.'))
             {
-                printf(RED
-                    "ERROR 2: El PESO solo puede contener numeros\n"YELLOW
-                    "Uso    : ./main [ID]:[PESO]:[CATEG]...[ID]:[PESO]:[CATEG]\n"
-                    "Ejemplo: ./main 101:2.5:A 104:4.0:B\n"RED
-                    "\n_________________________________________FAIL\n\n"RESET);
+                printf(RED"ERROR: El PESO solo puede contener numeros o punto\n"RESET);
+                verUso();
                 return 1;
             } 
+            if(argv[i][j] == '.')
+            {
+                cont_punto_float++;
+            }
+            if(argv[i][j] == '.' && argv[i][j + 1] == ':')
+            {
+                printf(RED"ERROR: El punto del float esta mal colocado. Falta parte decimal\n"RESET);
+                verUso();
+                return 1;
+            }
             j++;
+        }
+        if(cont_punto_float > 1)
+        {
+            printf(RED"ERROR: El float puede contener un unico punto\n"RESET);
+            verUso();
+            return 1;
         }
         if(argv[i][j] == ':')
         {
             cont_separadores++;
             j++;
         }
+        // CATEGORIA (CHAR)
         while(argv[i][j] != '\0')
         {
-            // Comprobamos la categoria es solo letras
+            // Comprobamos la categoria es A, B o C
             if(
-                argv[i][j] != 'A' && argv[i][j] != 'B' && argv[i][j] != 'C' ||
+                argv[i][j] != 'A' && argv[i][j] != 'B' && argv[i][j] != 'C' &&
                 argv[i][j] != 'a' && argv[i][j] != 'b' && argv[i][j] != 'c'
             )
             {
-                printf(RED
-                    "ERROR 3: La CATEG debe ser 'A', 'B' o 'C'\n"YELLOW
-                    "Uso    : ./main [ID]:[PESO]:[CATEG]...[ID]:[PESO]:[CATEG]\n"
-                    "Ejemplo: ./main 101:2.5:A 104:4.0:B\n"RED
-                    "\n_________________________________________FAIL\n\n"RESET);
+                printf(RED "ERROR 3: La CATEG debe ser 'A', 'B' o 'C'\n"RESET);
+                verUso();
                 return 1;
             } 
             j++;
@@ -134,9 +144,8 @@ int main(int argc, char *argv[])
         }
     }
 
-
-    // Comprobamos que el formato de los objetos es correcto 
     // Si son correctos los guardamos en nuestro array de estructuras
+    // Utilizamos un arreglo de string intermedio
 
     // Imprimimos los objetos en orden
 
@@ -165,6 +174,14 @@ int comprobarObjetos()
 void imprimirObjetos(char letra)
 {
 
+}
+
+void verUso()
+{
+    printf(YELLOW
+        "Uso  : ./main [ID]:[PESO]:[CATEG]...[ID]:[PESO]:[CATEG]\n"
+        "Ej.  : ./main 101:2.5:A 102:3:B 103:1:C\n"RED
+        "\n_________________________________________FAIL\n\n"RESET);
 }
 
 
