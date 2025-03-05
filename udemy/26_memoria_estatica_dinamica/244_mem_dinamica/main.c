ddcbf93e3fbf9f1527bf56eb7139cbb0dd54c4be
Stefan <trfstephan@gmail.com>
2025-03-05 23:20:33 +0100

    refactor: eliminar archivos obsoletos y agregar nuevas implementaciones de memoria dinámica

---
+++ udemy/26_memoria_estatica_dinamica/244_mem_dinamica/main.c
@@ -0,0 +1,67 @@
+/*
+    *   Autor
+        Stefan Trifan
+
+    *   Estado
+
+    *   Enunciado X
+        Lorem Ipsum
+*/
+
+/* _________________________________________
+   Inicio cabecera */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Funciones del programa
+
+// Funciones auxiliares
+void clearBuffer();
+
+/* _________________________________________
+   Inicio main() */
+
+int main(int argc, char *argv[])
+{
+    printf("\n_________________________________________START\n\n");
+
+    int *mi_entero;
+    mi_entero = (int *)malloc(sizeof(int));
+    if (mi_entero != NULL)
+    {
+        printf("Exito\n");
+    }
+    else
+    {
+        printf("Fracaso\n");
+    }
+
+    int* mi_array;
+    mi_array = (int *)malloc(sizeof(int)*4);
+    if(mi_array != NULL)
+    {
+        printf("Exito\n");
+    }
+    else
+    {
+        printf("Fracaso\n");
+    }
+    free(mi_array);
+    free(mi_entero);
+
+    printf("\n_________________________________________END\n\n");
+    return 0;
+}
+
+/* _________________________________________
+   Inicio definicion de funciones */
+
+// Funciones del programa
+
+// Funciones auxiliares
+void clearBuffer()
+{
+    while (getchar() != '\n')
+        ;
+}
