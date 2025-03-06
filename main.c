560ed889c75a6bdc791de1bda1a319db2b598e14
Stefan <trfstephan@gmail.com>
2025-03-06 09:33:38 +0100

    refactor: eliminar archivo ZIP obsoleto y simplificar el código en main.c

--- main.c
+++ main.c
@@ -1,65 +1,17 @@
-/* 
+/*
     *   Autor
         Stefan Trifan
 
     *   Estado
-        Done
+
+    *   Enunciado X
+        Lorem Ipsum
 */
 
+/* _________________________________________
+   Inicio cabecera */
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// Funciones del programa
-
-// Funciones auxiliares
-
-/* ___________________________________
-Inicio main */
-
-int main(int argc, char *argv[])
-{
-    printf("___________________________________END");
-    return 0;
-}
-
-/* ___________________________________
-Inicio definicion de funciones */
-
-// Funciones del programa
-
-// Funciones auxiliares
-void clearBuffer()
-{
-    while(getchar() != '\n');
-}
-
-void clearBuffer()
-{
-    while (getchar() != '\n');
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Funciones del programa
 
@@ -85,3 +37,7 @@ int main(int argc, char *argv[])
 // Funciones del programa
 
 // Funciones auxiliares
+void clearBuffer()
+{
+    while (getchar() != '\n');
+}
