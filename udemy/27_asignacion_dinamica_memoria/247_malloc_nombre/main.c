7fc8a5b5aaff307bc1fb18f2ea781677adb87de6
Stefan <trfstephan@gmail.com>
2025-03-15 10:19:58 +0100

    refactor: eliminar archivos obsoletos de plantillas y agregar nuevas plantillas

--- udemy/27_asignacion_dinamica_memoria/247_malloc_nombre/main.c
+++ udemy/27_asignacion_dinamica_memoria/247_malloc_nombre/main.c
@@ -27,7 +27,18 @@ int main(int argc, char *argv[])
 {
     printf("\n_________________________________________START\n\n");
 
-    
+    char nombre[20], *p_nombre;
+    int longitud;
+
+    strcpy(nombre, "Steve");
+    longitud = strlen(nombre);
+
+    p_nombre = (char *)malloc((longitud + 1) * sizeof(char));
+
+    strcpy(p_nombre, nombre);
+
+    printf("Longitud %d\n", longitud);
+    printf("Nombre %s\n", p_nombre);
 
     printf("\n_________________________________________END\n\n");
     return 0;
