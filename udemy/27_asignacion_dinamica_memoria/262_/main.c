683d11754f5bbc00a8102e54fa14bbfdb4a92d5b
Stefan <trfstephan@gmail.com>
2025-03-15 08:49:52 +0100

    refactor: agregar archivos de ejemplo para la gestión de memoria dinámica

---
+++ udemy/27_asignacion_dinamica_memoria/262_/main.c
@@ -0,0 +1,43 @@
+/*
+    *   Autor
+        Stefan Trifan
+
+    *   Estado
+
+    *   Enunciado X
+        Lorem Ipsum
+*/
+
+/* _________________________________________
+   Inicio cabecera */
+
+#include <stdio.h>
+
+// Funciones del programa
+
+// Funciones auxiliares
+void clearBuffer();
+
+/* _________________________________________
+   Inicio main() */
+
+int main(int argc, char *argv[])
+{
+    printf("\n_________________________________________START\n\n");
+
+
+
+    printf("\n_________________________________________END\n\n");
+    return 0;
+}
+
+/* _________________________________________
+   Inicio definicion de funciones */
+
+// Funciones del programa
+
+// Funciones auxiliares
+void clearBuffer()
+{
+    while (getchar() != '\n');
+}
