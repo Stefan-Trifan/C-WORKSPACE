a6ddee423c17a97812e8b1382ab82d98570f795e
Stefan <trfstephan@gmail.com>
2025-03-15 09:13:52 +0100

    refactor: agregar gestión de memoria dinámica en el archivo principal y eliminar archivos obsoletos

--- udemy/27_asignacion_dinamica_memoria/246_malloc/main.c
+++ udemy/27_asignacion_dinamica_memoria/246_malloc/main.c
@@ -12,6 +12,7 @@
    Inicio cabecera */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 // Funciones del programa
 
@@ -25,7 +26,18 @@ int main(int argc, char *argv[])
 {
     printf("\n_________________________________________START\n\n");
 
+    float *p_num;
+    p_num = (float *)malloc(sizeof(float));
+    if(p_num == NULL)
+    {
+        printf("Error\n");
+        return 0;
+    }
 
+    *p_num = 5.2;
+
+    printf("num %.2f\n", *p_num);
+    free(p_num);
 
     printf("\n_________________________________________END\n\n");
     return 0;
