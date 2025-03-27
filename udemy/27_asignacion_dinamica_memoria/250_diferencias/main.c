ce267848c103869bd4aa9719b67e2d8572bb45f0
Stefan <trfstephan@gmail.com>
2025-03-27 16:32:05 +0100

    refactor: agregar nuevo archivo main.c y eliminar archivo obsoleto en la asignación dinámica de memoria

--- udemy/27_asignacion_dinamica_memoria/250_diferencias/main.c
+++ udemy/27_asignacion_dinamica_memoria/250_diferencias/main.c
@@ -27,7 +27,28 @@ int main(int argc, char *argv[])
 {
     printf("\n_________________________________________START\n\n");
 
-    int *hola = (int *)malloc(sizeof(int));
+    int *hola = (int *)malloc(10*sizeof(int));
+
+    *hola = 5;
+
+    printf("hola = %p\n", hola);
+    printf("hola = %d\n", *hola);
+
+    printf("_________________________________\n\n");
+
+    int *adios = (int *)calloc(1, sizeof(int));
+
+    printf("adios = %p\n", adios);
+    printf("adios = %d\n", *adios);
+
+    printf("_________________________________\n\n");
+
+    hola = (int*)realloc(hola, 20 * sizeof(int));
+
+    *(hola + 19) = 10;
+
+    printf("hola = %p\n", hola + 19);
+    printf("hola = %d\n", *(hola + 19));
 
     printf("\n_________________________________________END\n\n");
     return EXIT_SUCCESS;
