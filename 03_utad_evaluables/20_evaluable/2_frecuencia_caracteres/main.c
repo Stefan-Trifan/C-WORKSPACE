4db36d0b2a11fb8f831ec605e5eb4106be8ffc91
Stefan <trfstephan@gmail.com>
2025-03-27 16:52:39 +0100

    refactor: agregar nuevos archivos main.c para múltiples evaluables y establecer estructura básica

---
+++ 03_utad_evaluables/20_evaluable/2_frecuencia_caracteres/main.c
@@ -0,0 +1,45 @@
+/*
+    *   Autor
+        Stefan Trifan
+
+    *   Estado
+
+    *   Enunciado 2 - Frecuencia de caracteres
+        Lorem Ipsum
+*/
+
+/* _________________________________________
+   Inicio cabecera */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Funciones del programa
+
+// Funciones auxiliares
+void clearBuffer();
+
+/* _________________________________________
+   Inicio main() */
+
+int main(int argc, char *argv[])
+{
+    printf("\n_________________________________________START\n\n");
+
+
+
+    printf("\n_________________________________________END\n\n");
+    return EXIT_SUCCESS;
+}
+
+/* _________________________________________
+   Inicio definicion de funciones */
+
+// Funciones del programa
+
+// Funciones auxiliares
+void clearBuffer()
+{
+    while (getchar() != '\n');
+}
