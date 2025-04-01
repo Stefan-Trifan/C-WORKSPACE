53a51e2a7ce7169cc78e44d7e69e1f92c24bffc4
Stefan <trfstephan@gmail.com>
2025-04-01 12:00:06 +0200

    eliminar archivos obsoletos y agregar implementación para el descubrimiento de América

--- 01_utad_temario/40_entrada_salida_archivos/04_p45_america/main.c
+++ 01_utad_temario/40_entrada_salida_archivos/04_p45_america/main.c
@@ -27,7 +27,30 @@ int main(int argc, char *argv[])
 {
     printf("\n_________________________________________START\n\n");
 
+    FILE *fd;
+    fd = fopen("descubrimiento_ameirica.txt", "w+");
 
+    char s1[10], s2[10], s3[10], s4[10];
+    int anio_descubrimiento = 0;
+
+    // Comprobamos si tiene extio
+	if(fd == NULL)
+	{
+		printf("Error al abrir el archivo\n");
+		return EXIT_FAILURE;
+	}
+
+    // Escribimos en el fichero
+    fprintf(fd,"%s %s %s %s %d", "America", "se", "descubrio", "en", 1492);
+
+    rewind(fd);
+
+    fscanf(fd,"%s %s %s %s %d", s1, s2, s3, s4, &anio_descubrimiento);
+
+    fclose(fd);
+
+    // Calculamos hace cuantos anios se descubrio America
+    printf("America se descubrio hace %d anos", 2025 - anio_descubrimiento);
 
     printf("\n_________________________________________EXIT\n\n");
     return EXIT_SUCCESS;
