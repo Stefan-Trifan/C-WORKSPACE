42a2b36c9665a1e1030186eaae410e33fa20c9da
Stefan <trfstephan@gmail.com>
2025-04-03 09:03:45 +0200

    eliminar archivos de ejemplo obsoletos y mejorar la implementación de la cola en el código

--- 03_utad_evaluables/20_evaluable/4_lista_comprav_v0.0/main.c
+++ 03_utad_evaluables/20_evaluable/4_lista_comprav_v0.0/main.c
@@ -14,24 +14,39 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define CAPACITY 10
+
 typedef enum 
 {
     agregar = 1, atender, mostrar, salir
 } 
 t_menu;
 
-typedef int t_queue;
+typedef struct
+{
+    char *nombre;
+    int posicion;
+}
+t_cliente;
+
+typedef struct 
+{
+    t_cliente *cliente;
+    int num_clientes;
+    int capacidad;
+}
+t_queue;
 
 // Funciones del programa
-void initQueue   (t_queue *q, int capacity);
-int  isFull      (t_queue *q); // No sirve
-int  isEmpty     (t_queue *q); 
+void initQueue(t_queue *q, int capacity);
+int  isFull(t_queue *q); // No sirve
+int  isEmpty (t_queue *q); 
 void resizeQueue (t_queue *q);
-void enqueue     (t_queue *q, char *name); 
-void dequeue     (t_queue *q); 
+void enqueue(t_queue *q, char *name); 
+void dequeue(t_queue *q); 
 void displayQueue(t_queue *q);
-void freeQueue   (t_queue *q);
-void menu        (t_queue *q);
+void freeQueue(t_queue *q);
+void menu(t_queue *q);
 
 // Funciones auxiliares
 void clearBuffer();
@@ -43,7 +58,24 @@ int main(int argc, char *argv[])
 {
     printf("\n_________________________________________START\n\n");
 
+    // Declaracion de variables
+    t_queue q;
     
+    // Funcion void initQueue(t_queue *q, int capacity);
+    initQueue(&q, CAPACITY);
+
+    // INICIO DEBUG LOG
+    printf("================ DEBUG LOG ================\n");
+
+    printf("q.capacidad = %d\n", q.capacidad);
+    printf("q.num_clientes = %d\n", q.num_clientes);
+    printf("sizeof(t_cliente) = %lu\n", sizeof(t_cliente));
+
+    for(int i = 0; i < q.num_clientes; i++)
+    {
+        
+    }
+    // FIN DEBUG LOG
 
     printf("\n_________________________________________EXIT\n\n");
     return EXIT_SUCCESS;
@@ -61,7 +93,10 @@ int main(int argc, char *argv[])
  */
 void initQueue(t_queue *q, int capacity)
 {
-
+    // Inicializa la cola
+    q->capacidad = capacity;
+    q->num_clientes = 0;
+    q->cliente = (t_cliente *)malloc(q->capacidad * sizeof(t_cliente));
 }
 
 /**
