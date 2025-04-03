52346b98832eaa6c4ee110a374d75253543720a5
Stefan <trfstephan@gmail.com>
2025-04-03 10:38:02 +0200

    renombrar variable de equipo a jugador y mejorar la gestión de entrada de usuarios

--- 01_utad_temario/40_entrada_salida_archivos/05_p46_futbol/main.c
+++ 01_utad_temario/40_entrada_salida_archivos/05_p46_futbol/main.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
     printf("\n_________________________________________START\n\n");
 
     // Declaracion de variables
-    datos_jugador_t *equipo = (datos_jugador_t *)malloc(sizeof(datos_jugador_t));
+    datos_jugador_t *jugador = (datos_jugador_t *)malloc(sizeof(datos_jugador_t));
     FILE *fd = fopen("equipos", "w+");
     int opcion = 0;
 
@@ -51,12 +51,20 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
+    // Introducimos jugadores
     do
     {
         printf("Quieres introducir un usuario?\n[0] Si\n[1] No\n-> ");
         opcion = pedirEnteroPos();
+
+        if(opcion == 0)
+        {
+            printf("Introduce el nombre del jugador\n-> ");
+            printf("Introduce el nombre del jugador\n-> ");
+            printf("Introduce el nombre del jugador\n-> ");
+        }
     } 
-    while (opcion != 0);
+    while (opcion != 1);
 
     printf("\n_________________________________________EXIT\n\n");
     return EXIT_SUCCESS;
@@ -68,25 +76,6 @@ int main(int argc, char *argv[])
 // Funciones del programa
 
 // Funciones auxiliares
-int pedirEnteroPos()
-{
-    int num = 0, esValido = 0;
-    do
-    {
-        esValido = scanf("%d", &num);
-        clearBuffer();
-        if (esValido == 0 || num < 0)
-        {
-            printf(
-                "\033[1;31mERROR: El tipo de dato introducido no es válido.\n"
-                "-> \033[0m"
-            );
-            esValido = 0;
-        }
-    }
-    while (esValido != 1);
-    return num;
-}
 
 char *pedirCadenaDinamica()
 {
@@ -108,6 +97,26 @@ char *pedirCadenaDinamica()
     p_texto_destino[i] = '\0';
 
     return p_texto_destino;
+}
+
+int pedirEnteroPos()
+{
+    int num = 0, esValido = 0;
+    do
+    {
+        esValido = scanf("%d", &num);
+        clearBuffer();
+        if (esValido == 0 || num < 0)
+        {
+            printf(
+                "\033[1;31mERROR: El tipo de dato introducido no es válido.\n"
+                "-> \033[0m"
+            );
+            esValido = 0;
+        }
+    }
+    while (esValido != 1);
+    return num;
 }
 
 void clearBuffer()
