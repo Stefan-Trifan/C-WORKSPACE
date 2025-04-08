b71c49c813c861270b5a2172aa10024a2c6a9467
Stefan <trfstephan@gmail.com>
2025-04-08 12:32:29 +0200

    cambios

--- 01_utad_temario/PR2_40_entrada_salida_archivos/06_p61_mostrar_lineas/main.c
+++ 01_utad_temario/PR2_40_entrada_salida_archivos/06_p61_mostrar_lineas/main.c
@@ -15,9 +15,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define TAM_BLOQUE 10
+
+typedef struct
+{
+    int    num_lineas;
+    char **lineas;
+}
+lineas_t;
+
 // Funciones del programa
 
 // Funciones auxiliares
+char *pedirCadenaDinamica();
 void clearBuffer();
 
 /* _________________________________________
@@ -39,6 +49,28 @@ int main(int argc, char *argv[])
 // Funciones del programa
 
 // Funciones auxiliares
+char *pedirCadenaDinamica()
+{
+    char *p_texto_destino = malloc(sizeof(char) * TAM_BLOQUE);
+    char c;
+    int i = 0;
+    int memoria_actual = TAM_BLOQUE;
+
+    while ((c = getchar()) != '\n')
+    {
+        if (i == memoria_actual)
+        {
+            memoria_actual += TAM_BLOQUE;
+            p_texto_destino = realloc(p_texto_destino, memoria_actual * sizeof(char));
+        }
+        p_texto_destino[i] = c;
+        i++;
+    }
+    p_texto_destino[i] = '\0';
+
+    return p_texto_destino;
+}
+
 void clearBuffer()
 {
     while (getchar() != '\n');
