c7acea860974b0320468933a85e538dc4a6861f3
Stefan <trfstephan@gmail.com>
2025-04-10 10:48:04 +0200

    cambios

--- 01_utad_temario/PR2_50_cadenas_en_profundidad/01_p10-1_apellido_resuelto/main.c
+++ 01_utad_temario/PR2_50_cadenas_en_profundidad/01_p10-1_apellido_resuelto/main.c
@@ -5,7 +5,7 @@
     *   Estado
 
     *   Enunciado
-        Lorem Ipsum
+        ./main Trifan
 */
 
 /* _________________________________________
@@ -14,6 +14,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define TAM_BLOQUE 10
 
 // Funciones del programa
 
@@ -27,7 +29,26 @@ int main(int argc, char *argv[])
 {
     printf("\n_________________________________________START\n\n");
 
+    if(argc != 2)
+    {
+        printf("Tienes que introducir parametros\n");
+        printf("\n_________________________________________FAIL\n\n");
+        return EXIT_FAILURE;
+    }
 
+    // Declaramos las variables
+    char *apellidoOriginal = *argv[1];
+    char *apellidoMinus;
+    char *apellidoMayus;
+
+    // Guardamos el apellido de argv a apellidoOriginal
+    printf("Apellido es: %s", *apellidoOriginal);
+
+    // Convertimos cada una de las letras en mayuscula y lo guardamos en apellidoMayus
+
+    // Convertimos cada una de las letras en minuscula y lo guaramos en apellidoMinus
+
+    // Imprimmos los apellidos en mayuscula y en minuscula
 
     printf("\n_________________________________________EXIT\n\n");
     return EXIT_SUCCESS;
