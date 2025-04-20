dccf79d7bdd558083657d7404f801ca350a2ae68
Stefan <trfstephan@gmail.com>
2025-04-20 17:02:45 +0200

    agregar archivo main.c con funcionalidad para escribir en un archivo y verificar su creación

--- udemy/20_manejo_de_archivos/194_escribir_texto/main.c
+++ udemy/20_manejo_de_archivos/194_escribir_texto/main.c
@@ -27,7 +27,26 @@ int main(int argc, char *argv[])
 {
     printf("\n_________________________________________START\n\n");
 
+    // Declaramos variables
+    FILE *fd;
+    char texto[] = "Hola Mundo";
+    
+    // Abrimps el archivo en modo escritura (posicion final)
+    fd = fopen("prueba.txt", "a");
 
+    // Comprobamos si se ha creado
+    if(fd == NULL)
+    {
+        printf("No creado\n");
+    }
+    else
+    {
+        printf("Creado\n");
+        fprintf(fd, "Texto escrito: 123 ");
+    }
+
+    // Cerramos el archivo
+    fclose(fd);
 
     printf("\n_________________________________________EXIT\n\n");
     return EXIT_SUCCESS;
