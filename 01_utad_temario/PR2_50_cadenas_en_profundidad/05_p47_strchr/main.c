6b1af12d6e7282331ee184d561db00c43ea566fe
Stefan <trfstephan@gmail.com>
2025-04-24 10:31:59 +0200

    changes

--- 01_utad_temario/PR2_50_cadenas_en_profundidad/05_p47_strchr/main.c
+++ 01_utad_temario/PR2_50_cadenas_en_profundidad/05_p47_strchr/main.c
@@ -15,8 +15,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Funciones del programa
+#define TAM_BLOQUE 10
 
+// Funciones del programa
+char *pedirCadenaDinamica();
 // Funciones auxiliares
 void clearBuffer();
 
@@ -27,7 +29,16 @@ int main(int argc, char *argv[])
 {
     printf("\n_________________________________________START\n\n");
 
+    // Declaracion de variables
+    char *cadena;
+    char letra;
 
+    // Pedimos la cadena y el caracter
+    printf("Introduce la cadena de caracteres: ");
+    cadena = pedirCadenaDinamica();
+
+    printf("Introduce la letra: ");
+    scanf("%c", letra);
 
     printf("\n_________________________________________EXIT\n\n");
     return EXIT_SUCCESS;
@@ -37,6 +48,27 @@ int main(int argc, char *argv[])
    Inicio definicion de funciones */
 
 // Funciones del programa
+char *pedirCadenaDinamica()
+{
+    char *p_texto_destino = malloc(sizeof(char) * TAM_BLOQUE);
+    char c;
+    int i = 0;
+    int memoria_actual = TAM_BLOQUE;
+
+    while ((c = getchar()) != '\n')
+    {
+        if (i == memoria_actual)
+        {
+            memoria_actual += TAM_BLOQUE;
+            p_texto_destino = realloc(p_texto_destino, memoria_actual * sizeof(char));
+        }
+        p_texto_destino[i] = c;
+        i++;
+    }
+    p_texto_destino[i] = '\0';
+
+    return p_texto_destino;
+}
 
 // Funciones auxiliares
 void clearBuffer()
