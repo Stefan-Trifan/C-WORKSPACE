0e8a1fa9f86926f540d57ccfd018959622af64b0
Stefan <trfstephan@gmail.com>
2025-04-24 10:02:12 +0200

    changes

--- 01_utad_temario/PR2_50_cadenas_en_profundidad/03_p39_strcpy_strcat_fichero/main.c
+++ 01_utad_temario/PR2_50_cadenas_en_profundidad/03_p39_strcpy_strcat_fichero/main.c
@@ -30,7 +30,20 @@ int main(int argc, char *argv[])
 {
     printf("\n_________________________________________START\n\n");
 
+    // Comprobamos que el programa recibe los parametros
+    if(argc != 2)
+    {
+        printf("ERROR: Tienes que introducir el nombre del fichero.\n");
+        printf("USO: ./main datos.txt");
+        printf("\n_________________________________________FAIL\n\n");
+        return EXIT_FAILURE;
+    }
+
     // Declaracion de variables
+    char *nombre_fichero;
+
+    // Calculamos la longitud del nombre fichero
+    printf("Tam nombre fichero: %d", strlen(argv[1]));
 
     printf("\n_________________________________________EXIT\n\n");
     return EXIT_SUCCESS;
