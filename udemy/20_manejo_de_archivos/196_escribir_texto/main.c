8701c009956bded961ca535e94400a9635831660
Stefan <trfstephan@gmail.com>
2025-04-26 17:57:35 +0200

    cambios nomenclatura

---
+++ udemy/20_manejo_de_archivos/196_escribir_texto/main.c
@@ -0,0 +1,68 @@
+/*
+    *   Autor
+        Stefan Trifan
+
+    *   Estado
+
+    *   Enunciado
+        Lorem Ipsum
+*/
+
+/* _________________________________________
+   Inicio cabecera */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Funciones del programa
+
+// Funciones auxiliares
+void clearBuffer();
+
+/* _________________________________________
+   Inicio main() */
+
+int main(int argc, char *argv[])
+{
+    printf("\n_________________________________________START\n\n");
+
+    // Declaracion de variables
+    FILE *fd;
+    char c;
+
+    // Abrimos el archivo
+    fd = fopen("datos.txt", "r+");
+
+    // Comprobamos que se ha abierto bien
+    if(fd == NULL)
+    {
+        printf("No existe el aschivo");
+        printf("\033[31m\n_________________________________________FAIL\n\n\033[0m");
+        return EXIT_FAILURE;
+    }
+
+    // Mientras que el usuario no presione Enter
+    printf("-> ");
+    while((c = getchar()) != '\n')
+    {
+        fputc(c, fd);
+    }
+
+    // Cerramos el archivo
+    fclose(fd);
+
+    printf("\n_________________________________________EXIT\n\n");
+    return EXIT_SUCCESS;
+}
+
+/* _________________________________________
+   Inicio definicion de funciones */
+
+// Funciones del programa
+
+// Funciones auxiliares
+void clearBuffer()
+{
+    while (getchar() != '\n');
+}
