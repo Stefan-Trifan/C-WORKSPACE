f3fb0db051cdde8bc5dcd9c7694f3a57e4b4e49f
Stefan <trfstephan@gmail.com>
2025-05-01 14:23:20 +0200

    nuevos ejercicios

--- udemy/22_ejercicios_de_cadenas/203_funcion_strcpy/main.c
+++ udemy/22_ejercicios_de_cadenas/203_funcion_strcpy/main.c
@@ -28,7 +28,20 @@ int main(int argc, char *argv[])
     printf("\n_________________________________________START\n\n");
 
     // Declaracion de variables
+    char origen[] = "Hola Mundo";
+    char destino[30];
+    
+    // Copiamos
+    strcpy(destino, origen);
+    
+    // Imprimimos
+    printf("destino: %s\n", destino);
+    
+    // Cambiamos
+    strcpy(destino + 5, "soy el mejor en C jeje");
 
+    // Imprimmos nuevamente
+    printf("destino: %s\n", destino);
 
     printf("\n_________________________________________EXIT\n\n");
     return EXIT_SUCCESS;
