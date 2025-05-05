df358504a36bf3f2437c8677f821db1c19884716
Stefan <trfstephan@gmail.com>
2025-05-05 16:58:19 +0200

    agregar validación de argumentos y definir funciones para el análisis de palabras en el fichero

--- 03_utad_evaluables/P2_3_evaluable/1_analisis_palabras_fichero/main.c
+++ 03_utad_evaluables/P2_3_evaluable/1_analisis_palabras_fichero/main.c
@@ -15,7 +15,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct {} listaPalabras_t;
+
 // Funciones del programa
+void examinaLinea(char *lineaAux, int numLinea, listaPalabras_t listaPalabras);
+char *leeLineaDinamicaFile(FILE *fd);
+void escribirResultado(listaPalabras_t lista, char *nombreFichOrigen);
 
 // Funciones auxiliares
 void clearBuffer();
@@ -26,6 +31,13 @@ void clearBuffer();
 int main(int argc, char *argv[])
 {
     printf("\n_________________________________________START\n\n");
+
+    if(argc != 3)
+    {
+        printf("USO: ./main <Nombre de fichero> <Palabras a buscar>\n");
+        printf("\033[31m\n_________________________________________FAIL\n\n\033[0m");
+        return EXIT_FAILURE;
+    }
 
     // Declaracion de variables
 
@@ -38,6 +50,34 @@ int main(int argc, char *argv[])
    Inicio definicion de funciones */
 
 // Funciones del programa
+/**
+ * Esta función recibe una línea y el número de línea y modifica 
+ * la lista de palabras para añadir la información encontrada. 
+ */
+void examinaLinea(char *lineaAux, int numLinea, listaPalabras_t listaPalabras)
+{
+
+}
+
+/**
+ * Esta función obtiene la siguiente línea de un fichero ya abierto y devuelve 
+ * un puntero a la cadena con memoria reservada dinámicamente. Utiliza fgetc
+ */
+char *leeLineaDinamicaFile(FILE *fd)
+{
+
+}
+
+/**
+ * Abre el fichero de escritura obteniendo el nombre a partir del nombre 
+ * del fichero origen y escribe para cada palabra encontrada: la palabra, 
+ * el número de ocurrencias y las líneas en las que aparece, si una 
+ * palabra aparece varias veces en una línea aparecerá duplicada en la lista
+ */
+void escribirResultado(listaPalabras_t lista, char *nombreFichOrigen)
+{
+
+}
 
 // Funciones auxiliares
 void clearBuffer()
